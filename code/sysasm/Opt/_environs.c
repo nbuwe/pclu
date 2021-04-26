59e4a7b7a41b352d928f6f1190fb0bf4e0378818
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-04-26 05:35:58 +0300

    sysasm: _environs.c - cleanup
    ANSIfy and fix warnings.  Prettify.
    G/c RCS Header and Log.

--- code/sysasm/Opt/_environs.c
+++ code/sysasm/Opt/_environs.c
@@ -1,67 +1,57 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _environs.c,v 1.5 91/07/09 13:08:53 root Exp $";
-#endif
-/* $Log:	_environs.c,v $
- * Revision 1.5  91/07/09  13:08:53  root
- * changed args to stringOPcons to CLUREFs (sun compatibility)
- * 
- * Revision 1.4  91/06/06  13:24:51  root
- * added copyright notice
- * 
- * Revision 1.3  91/05/31  11:51:16  root
- * fixed checkin comment
- * 
- * Revision 1.2  91/05/31  11:50:38  root
- * removed unused variable declaration
- * 
- * Revision 1.1  91/02/04  15:49:33  mtv
- * Initial revision
- * 
- */
-
 
 /*							*/
 /*							*/
 /*		IMPLEMENTATION OF			*/
-/*			_environ			*/
+/*			_environs			*/
 /*	an iterator that yields the environment strings */
 /*							*/
 
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
+#include <string.h>
+
+/* iterator body callback */
+typedef errcode (*iter_body_t)(CLUREF iv_1, void *locals, errcode *iecode);
+
+errcode stringOPcons(const char *buf, CLUREF start, CLUREF len, CLUREF *ans);
+
+/* set by the C startup code in util.c */
 extern char **envp;
 
-errcode _environs(iterbod, locals, iecode)
-char * locals;
-errcode (*iterbod)();
-errcode *iecode;
+
+/*
+ * _environs = iter () yields (string)
+ */
+errcode
+_environs(iter_body_t ib, void *locals, errcode *iecode)
 {
-errcode err;
-CLUREF iv_1, len;
-int index;
+    CLUREF iv_1;
+    errcode err;
 
-	if (envp[0] == NULL) signal(ERR_ok);
-	index = 0;
-	for (index = 0; envp[index] != NULL; index++) {
-		len.num = strlen(envp[index]);
-		err = stringOPcons(envp[index], CLU_1, len, &iv_1);
-		if (err != ERR_ok) resignal(err);
-		err = iterbod(iv_1, locals, iecode);
-		if (err == ERR_ok) continue;
-		/* 9/9/93 dwc: just propagate break, do not convert to ok */
-		if (err == ERR_break) signal(ERR_break);
-		if (err == ERR_iterbodyexit) signal(err);
-		if (err == ERR_iterbodyreturn) signal(err);
-		if (err == ERR_iterbodysignal) signal(err);
-		if (err == ERR_iteriterbodyexit) signal(err);
-		if (err == ERR_iteriterbodyreturn) signal(err);
-		if (err == ERR_iteriterbodysignal) signal(err);
-		}
+    if (envp == NULL || envp[0] == NULL)
 	signal(ERR_ok);
-	}
 
+    for (size_t i = 0; envp[i] != NULL; ++i) {
+	size_t len = strlen(envp[i]);
+	err = stringOPcons(envp[i], CLU_1, CLUREF_make_num(len), &iv_1);
+	if (err != ERR_ok)
+	    resignal(err);
 
+	err = (*ib)(iv_1, locals, iecode);
+	if (err == ERR_ok)
+	    continue;
+
+	/* 9/9/93 dwc: just propagate break, do not convert to ok */
+	if (err == ERR_break) signal(err);
+	if (err == ERR_iterbodyexit) signal(err);
+	if (err == ERR_iterbodysignal) signal(err);
+	if (err == ERR_iterbodyreturn) signal(err);
+	if (err == ERR_iteriterbodyexit) signal(err);
+	if (err == ERR_iteriterbodysignal) signal(err);
+	if (err == ERR_iteriterbodyreturn) signal(err);
+    }
+
+    signal(ERR_ok);
+}
