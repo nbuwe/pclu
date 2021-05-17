d216fe2cb60180518c9ca6d60d215efff83a9e21
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-05-18 00:25:29 +0300

    sysasm: type.c - ANSIfy, prettify.

--- code/sysasm/Opt/type.c
+++ code/sysasm/Opt/type.c
@@ -1,18 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: type.c,v 1.2 91/06/06 13:31:44 root Exp $";
-#endif
-/* $Log:	type.c,v $
- * Revision 1.2  91/06/06  13:31:44  root
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  15:49:55  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -23,26 +9,32 @@ static char rcsid[] = "$Header: type.c,v 1.2 91/06/06 13:31:44 root Exp $";
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-errcode typeOPequal(t1, t2, ans)
-CLUREF t1, t2, *ans;
-{
-	if (t1.num == t2.num) ans->tf = true;
-	else ans->tf = false;
-	signal(ERR_ok);
-	}
 
-errcode typeOPsimilar(t1, t2, ans)
-CLUREF t1, t2, *ans;
+errcode
+typeOPequal(CLUREF t1, CLUREF t2, CLUREF *ans)
 {
-	if (t1.num == t2.num) ans->tf = true;
-	else ans->tf = false;
-	signal(ERR_ok);
-	}
+    if (t1.num == t2.num)
+	ans->tf = true;
+    else
+	ans->tf = false;
+    signal(ERR_ok);
+}
 
-errcode typeOPcopy(t, ans)
-CLUREF t, *ans;
+
+errcode
+typeOPsimilar(CLUREF t1, CLUREF t2, CLUREF *ans)
 {
-	ans->num = t.num;
-	signal(ERR_ok);
-	}
+    if (t1.num == t2.num)
+	ans->tf = true;
+    else
+	ans->tf = false;
+    signal(ERR_ok);
+}
 
+
+errcode
+typeOPcopy(CLUREF t, CLUREF *ans)
+{
+    ans->num = t.num;
+    signal(ERR_ok);
+}
