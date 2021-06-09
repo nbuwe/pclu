96a3b50ac51e58cecc27a247d38af6e89617f8b2
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-10 00:48:48 +0300

    libasm: _set_alarm.c - ANSIfy, prettify.  Use sigfillset().

--- code/libasm/Opt/_set_alarm.c
+++ code/libasm/Opt/_set_alarm.c
@@ -1,17 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _set_alarm.c,v 1.2 91/06/06 13:50:33 dcurtis Exp $";
-#endif
-/* $Log:	_set_alarm.c,v $
- * Revision 1.2  91/06/06  13:50:33  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:20:57  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*		IMPLEMENTATION OF		*/
@@ -20,52 +7,52 @@ static char rcsid[] = "$Header: _set_alarm.c,v 1.2 91/06/06 13:50:33 dcurtis Exp
 
 #include <signal.h>
 #undef signal
+
 #include "pclu_err.h"
 #include "pclu_sys.h"
+
 #include <sys/time.h>
 #include <errno.h>
 
-extern char **environ;
+static void CLU_alarm(int);
 
-static bool itimer_flag = false;
-extern void clu_alarm();
+static volatile bool itimer_flag = false;
 
-errcode _set_alarm(secs, ans)
-CLUREF secs, *ans;
+
+errcode
+_set_alarm(CLUREF secs, CLUREF *ans)
 {
-int uerr;
-char *addr;
-errcode err;
-CLUREF temp;
-struct sigaction vec, ovec;
-struct itimerval value;
-int which;
+    int uerr;
+    char *addr;
+    errcode err;
+    CLUREF temp;
+    struct sigaction vec;
+    struct itimerval value;
+    int which;
 
-	if (secs.num > 0) {
-		vec.sa_handler = clu_alarm;
-#if defined(LINUX) && !defined(OLD_LINUX)
-		vec.sa_mask.__val[0] = -1;
-#else
-		vec.sa_mask = -1;
-#endif
-		vec.sa_flags = 1;
-		uerr = sigaction(SIGALRM, &vec, &ovec);
-		}
-	which = ITIMER_REAL;
-	value.it_interval.tv_sec = 0;
-	value.it_interval.tv_usec = 0;
-	value.it_value.tv_sec = secs.num;
-	value.it_value.tv_usec = 0;
-	ans->tf = itimer_flag;
-	itimer_flag = false;
-	uerr = setitimer(which, &value, 0);
-	signal(ERR_ok);
-	}
+    if (secs.num > 0) {
+	vec.sa_handler = clu_alarm;
+	sigfillset(&vec.sa_mask);
+	vec.sa_flags = 1;
+	uerr = sigaction(SIGALRM, &vec, NULL);
+    }
 
-void clu_alarm(sig, code, scp)
-unsigned int sig, code;
-struct sigcontext *scp;
+    which = ITIMER_REAL;
+    value.it_interval.tv_sec = 0;
+    value.it_interval.tv_usec = 0;
+    value.it_value.tv_sec = secs.num;
+    value.it_value.tv_usec = 0;
+
+    ans->tf = itimer_flag;
+    itimer_flag = false;
+
+    uerr = setitimer(which, &value, 0);
+    signal(ERR_ok);
+}
+
+
+static void
+CLU_alarm(int sig)
 {
-	itimer_flag = true;
-	return;
-	}
+    itimer_flag = true;
+}
