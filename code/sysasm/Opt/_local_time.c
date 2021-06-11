58c7a6b2c331edc77bf585751e60b3bd5aa1fd87
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-11 04:15:40 +0300

    sysasm: _local_time.c - ANSIfy, prettify.

--- code/sysasm/Opt/_local_time.c
+++ code/sysasm/Opt/_local_time.c
@@ -1,22 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _local_time.c,v 1.3 91/06/06 13:25:49 root Exp $";
-#endif
-/* $Log:	_local_time.c,v $
- * Revision 1.3  91/06/06  13:25:49  root
- * added copyright notice
- * 
- * Revision 1.2  91/06/03  11:24:34  root
- * sparcstation compatibility: int->CLUREF
- * 	in call to date$create
- * 
- * Revision 1.1  91/02/04  15:49:36  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -26,34 +8,43 @@ static char rcsid[] = "$Header: _local_time.c,v 1.3 91/06/06 13:25:49 root Exp $
 
 #include <time.h>
 #undef signal
+
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
 #include <errno.h>
 
-errcode _local_time(left, right, ans)
-CLUREF left, right, *ans;
+
+errcode
+_local_time(CLUREF left, CLUREF right, CLUREF *ans)
 {
-struct tm *tm;
-errcode err;
-CLUREF temp, day, mon, year, hour, min, sec;
-time_t clock;
+    errcode err;
 
-	clock = (left.num << 16) + right.num;
-	tm = localtime(&clock);
-	if (tm == NULL) {
-	    elist[0] = _unix_erstr(errno);
-	    signal(ERR_not_possible);
-	}
-	day.num = tm->tm_mday;
-	mon.num = tm->tm_mon + 1;
-	year.num = tm->tm_year + 1900;
-	hour.num = tm->tm_hour;
-	min.num = tm->tm_min;
-	sec.num = tm->tm_sec;
-	err = dateOPcreate(day, mon, year, hour, min, sec, &temp);
-	if (err != ERR_ok) resignal(err);
-	ans->num = temp.num;
-	signal(ERR_ok);
-	}
+    /*
+     * XXX: TODO: this days we should worry about 64-bit time_t on
+     * 32-bit hosts, not about 32-bit time_t on 16-bit hosts.
+     */
+    time_t clock = (left.num << 16) + right.num;
 
+    struct tm *tm = localtime(&clock);
+    if (tm == NULL) {
+	elist[0] = _unix_erstr(errno);
+	signal(ERR_not_possible);
+    }
+
+    CLUREF day, mon, year, hour, min, sec;
+    day.num = tm->tm_mday;
+    mon.num = tm->tm_mon + 1;
+    year.num = tm->tm_year + 1900;
+    hour.num = tm->tm_hour;
+    min.num = tm->tm_min;
+    sec.num = tm->tm_sec;
+
+    CLUREF temp;
+    err = dateOPcreate(day, mon, year, hour, min, sec, &temp);
+    if (err != ERR_ok)
+	resignal(err);
+
+    *ans = temp;
+    signal(ERR_ok);
+}
