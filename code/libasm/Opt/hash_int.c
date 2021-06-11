65c2aef1b7fcbe57f42daf4d8cfcc89926f0bf63
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-11 19:52:46 +0300

    libasm: hash_int.c - ANSIfy, prettify, clarify.
    Add a comment explaining that the unsigned cast is a workaround for
    floored division mandated by the C standard.  Drop the remainder test
    and adjustment, it cannot happen with unsigned division.

--- code/libasm/Opt/hash_int.c
+++ code/libasm/Opt/hash_int.c
@@ -1,18 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: hash_int.c,v 1.2 91/06/06 13:54:18 dcurtis Exp $";
-#endif 
-/* $Log:	hash_int.c,v $
- * Revision 1.2  91/06/06  13:54:18  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:21:28  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -23,14 +9,23 @@ static char rcsid[] = "$Header: hash_int.c,v 1.2 91/06/06 13:54:18 dcurtis Exp $
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-errcode hash_int(i, j, ans)
-CLUREF i, j, *ans;
+#include <stdint.h>
+
+
+errcode
+hash_int(CLUREF i, CLUREF max, CLUREF *ans)
 {
-	if (j.num == 0) {
-		ans->num = 0;
-		signal(ERR_ok);
-		}
-	ans->num = i.num % (unsigned) (j.num);
-        if (ans->num < 0) ans->num += j.num;          /* Added by MTV */
-	signal(ERR_ok);
-	}
+    if (max.num == 0) {
+	ans->num = 0;
+    }
+    else {
+	/*
+	 * The C standard specifies symmetric division (truncation
+	 * towards zero) that can produce negative remainders with
+	 * signed operands, so use unsigned.
+	 */
+	ans->num = (uintptr_t)i.num % (uintptr_t)max.num;
+    }
+
+    signal(ERR_ok);
+}
