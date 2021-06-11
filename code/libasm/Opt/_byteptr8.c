158c991df0a423e808f60bf88f9bd08196e4e1ac
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-11 17:12:37 +0300

    libasm: _byteptr8.c - ANSIfy, prettify.  Same object code is generated.

--- code/libasm/Opt/_byteptr8.c
+++ code/libasm/Opt/_byteptr8.c
@@ -1,23 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _byteptr8.c,v 1.4 91/06/06 13:43:35 dcurtis Exp $";
-#endif
-/* $Log:	_byteptr8.c,v $
- * Revision 1.4  91/06/06  13:43:35  dcurtis
- * added copyright notice
- * 
- * Revision 1.3  91/06/05  13:25:10  root
- * added elist to sequence$new2
- * 
- * Revision 1.2  91/06/03  17:06:40  root
- * sparcstation compatibility: int->CLUREF
- * 
- * Revision 1.1  91/02/04  23:20:13  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*						*/
@@ -30,265 +11,313 @@ static char rcsid[] = "$Header: _byteptr8.c,v 1.4 91/06/06 13:43:35 dcurtis Exp 
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-extern errcode _rtn_infoOPis_proc();
-extern errcode _rtn_infoOPis_iter();
-extern errcode _printn();
 
-extern errcode _wordvecOP_gcd();
-extern errcode intOP_gcd();
+// insert = proc (tab: cvt, z: int, inf: ginfo, x: _obj) returns (int)
+extern errcode gcd_tabOPinsert(CLUREF tab, CLUREF z, CLUREF inf, CLUREF x, CLUREF *ans);
+
+// _gcd = proc (self: cvt, tab: gcd_tab) returns (int)
+extern errcode _wordvecOP_gcd(CLUREF self, CLUREF tab, CLUREF *ans);
 extern OWN_ptr _wordvec_own_init;
+
+extern errcode intOP_gcd(CLUREF self, CLUREF tab, CLUREF *ans);
 extern OWN_ptr int_own_init;
 
+
+// rep = record
 typedef struct {
-CLUTYPE typ;
-CLUREF buf;			/* wordvec */
-CLUREF idx;
-CLUREF max;
+    CLUTYPE typ;
+    CLUREF buf;		// : wordvec
+    CLUREF idx;		// : int
+    CLUREF max;		// : int
 } WP;
 
-errcode _byteptr8OPcreate(wv, i, cnt, ans)
-CLUREF wv, i, cnt, *ans;
+
+errcode
+_byteptr8OPcreate(CLUREF wv, CLUREF i, CLUREF cnt, CLUREF *ans)
 {
-  int size = wv.vec->size;
-int len;
-WP *temp;
-CLUREF t;
+    int size = wv.vec->size;
+    int len;
+    WP *temp;
+    CLUREF t;
 
-	if (i.num - 1 > size) signal(ERR_bounds);
-	if (cnt.num >= 0) signal(ERR_negative_size);
+    if (i.num - 1 > size)
+	signal(ERR_bounds);
 
-/* this code ought to take the size of the rest of the wordvec, not the
-	size of the wordvec */
+    if (cnt.num >= 0)
+	signal(ERR_negative_size);
 
-	len = cnt.num + i.num - 1;
-	if (len > size) len = size;
-	clu_alloc(sizeof(WP), &temp);
-	temp->typ.val = 0;
-	temp->typ.mark = 0;
-	temp->typ.refp = 0;
-	temp->buf.vec = wv.vec;
-	temp->idx.num = i.num + 2;
-	temp->max.num = len + 3;
-	t = *((CLUREF*)&temp);
-	ans->vec = t.vec;
-	signal(ERR_ok);
+    /* this code ought to take the size of the rest of the wordvec,
+       not the size of the wordvec */
+    len = cnt.num + i.num - 1;
+    if (len > size)
+	len = size;
+
+    clu_alloc(sizeof(WP), &temp);
+    temp->typ.val = 0;
+    temp->typ.mark = 0;
+    temp->typ.refp = 0;
+
+    temp->buf.vec = wv.vec;
+    temp->idx.num = i.num + 2;
+    temp->max.num = len + 3;
+
+    t = *((CLUREF *)&temp);
+    ans->vec = t.vec;
+    signal(ERR_ok);
 }
 
-errcode _byteptr8OPreset(bp, wv, i, cnt)
-CLUREF bp, wv, i, cnt;
+
+errcode
+_byteptr8OPreset(CLUREF bp, CLUREF wv, CLUREF i, CLUREF cnt)
 {
-WP *b = (WP*)bp.ref;
-int size = wv.vec->size;
-int len;
+    WP *b = (WP *)bp.ref;
+    int size = wv.vec->size;
+    int len;
 
-	if (i.num - 1 > size) signal(ERR_bounds);
-	if (cnt.num < 0) signal(ERR_negative_size);
+    if (i.num - 1 > size)
+	signal(ERR_bounds);
 
-/* this code ought to take the size of the rest of the wordvec, not the
-	size of the wordvec */
+    if (cnt.num < 0)
+	signal(ERR_negative_size);
 
-	len = cnt.num + i.num - 1;
-	if (len > size) len = size;
-	b->buf.vec = wv.vec;
-	b->idx.num = i.num + 2;
-	b->max.num = len + 3;
-	signal(ERR_ok);
-	}
+    /* this code ought to take the size of the rest of the wordvec,
+       not the size of the wordvec */
+    len = cnt.num + i.num - 1;
+    if (len > size)
+	len = size;
 
-errcode _byteptr8OPnext(bp, ans)
-CLUREF bp, *ans;
+    b->buf.vec = wv.vec;
+    b->idx.num = i.num + 2;
+    b->max.num = len + 3;
+    signal(ERR_ok);
+}
+
+
+errcode
+_byteptr8OPnext(CLUREF bp, CLUREF *ans)
 {
-WP *b;
+    WP *b = (WP *)bp.ref;
 
-	b = (WP*)bp.ref;
-	if (b->idx.num + 1 > b->max.num) signal(ERR_bounds);
-	b->idx.num += 1;
-	ans->word = b->buf.vec->data[b->idx.num];
-	signal(ERR_ok);
-	}
+    if (b->idx.num + 1 > b->max.num)
+	signal(ERR_bounds);
 
-errcode _byteptr8OPpeek(bp, ans)
-CLUREF bp, *ans;
+    b->idx.num += 1;
+    ans->word = b->buf.vec->data[b->idx.num];
+    signal(ERR_ok);
+}
+
+
+errcode
+_byteptr8OPpeek(CLUREF bp, CLUREF *ans)
 {
-WP *b;
+    WP *b = (WP *)bp.ref;
 
-	b = (WP*)bp.ref;
-	if (b->idx.num + 1 > b->max.num) signal(ERR_bounds);
-	ans->word = b->buf.vec->data[b->idx.num + 1];
-	signal(ERR_ok);
-	}
+    if (b->idx.num + 1 > b->max.num)
+	signal(ERR_bounds);
 
-errcode _byteptr8OPdeposit(bp, c)
-CLUREF bp, c;
+    ans->word = b->buf.vec->data[b->idx.num + 1];
+    signal(ERR_ok);
+}
+
+
+errcode
+_byteptr8OPdeposit(CLUREF bp, CLUREF c)
 {
-WP *b = (WP*)bp.ref;
+    WP *b = (WP *)bp.ref;
 
-	if ((b->idx.num + 1) > b->max.num) signal(ERR_bounds);
-	b->idx.num += 1;
-	b->buf.vec->data[b->idx.num] = c.word;
-	signal(ERR_ok);
-	}
+    if ((b->idx.num + 1) > b->max.num)
+	signal(ERR_bounds);
 
-errcode _byteptr8OPadjust(bp, disp)
-CLUREF bp, disp;
+    b->idx.num += 1;
+    b->buf.vec->data[b->idx.num] = c.word;
+    signal(ERR_ok);
+}
+
+
+errcode
+_byteptr8OPadjust(CLUREF bp, CLUREF disp)
 {
-WP *b;
-int newidx;
+    WP *b = (WP *)bp.ref;
 
-	b = (WP*)bp.ref;
-	newidx = disp.num + b->idx.num;
-	if (newidx < 2 || newidx >b->max.num) signal(ERR_bounds);
-	b->idx.num = newidx;
-	signal(ERR_ok);
-	}
+    int newidx = disp.num + b->idx.num;
+    if (newidx < 2 || newidx >b->max.num)
+	signal(ERR_bounds);
 
-errcode _byteptr8OPempty(bp, ans)
-CLUREF bp, *ans;
+    b->idx.num = newidx;
+    signal(ERR_ok);
+}
+
+
+errcode
+_byteptr8OPempty(CLUREF bp, CLUREF *ans)
 {
-WP *b = (WP*)bp.ref;
+    WP *b = (WP *)bp.ref;
 
-	if (b->idx.num == b->max.num) ans->tf = true;
-	else ans->tf = false;
-	signal(ERR_ok);
-	}
+    if (b->idx.num == b->max.num)
+	ans->tf = true;
+    else
+	ans->tf = false;
+    signal(ERR_ok);
+}
 
-errcode _byteptr8OPget_buf(bp, ans)
-CLUREF bp, *ans;
+
+errcode
+_byteptr8OPget_buf(CLUREF bp, CLUREF *ans)
 {
-WP *b= (WP*)bp.ref;
+    WP *b= (WP *)bp.ref;
 
-	ans->vec = b->buf.vec;
-	signal(ERR_ok);
-	}
+    ans->vec = b->buf.vec;
+    signal(ERR_ok);
+}
 
-errcode _byteptr8OPget_index(bp, ans)
-CLUREF bp, *ans;
+
+errcode
+_byteptr8OPget_index(CLUREF bp, CLUREF *ans)
 {
-WP *b = (WP*)bp.ref;
+    WP *b = (WP*)bp.ref;
 
-	ans->num = b->idx.num - 2;
-	signal(ERR_ok);
-	}
+    ans->num = b->idx.num - 2;
+    signal(ERR_ok);
+}
 
-errcode _byteptr8OPget_count(bp, ans)
-CLUREF bp, *ans;
+
+errcode
+_byteptr8OPget_count(CLUREF bp, CLUREF *ans)
 {
-WP *b = (WP*)bp.ref;
+    WP *b = (WP*)bp.ref;
 
-	ans->num = b->max.num - b->idx.num;
-	signal(ERR_ok);
-	}
+    ans->num = b->max.num - b->idx.num;
+    signal(ERR_ok);
+}
 
-errcode _byteptr8OPequal(bp1, bp2, ans)
-CLUREF bp1, bp2, *ans;
+
+errcode
+_byteptr8OPequal(CLUREF bp1, CLUREF bp2, CLUREF *ans)
 {
+    if (bp1.num == bp2.num)
+	ans->tf = true;
+    else
+	ans->tf = false;
+    signal(ERR_ok);
+}
 
-	if (bp1.num == bp2.num) ans->tf = true;
-	else ans->tf = false;
-	signal(ERR_ok);
-	}
 
-
-errcode _byteptr8OPsimilar(bp1, bp2, ans)
-CLUREF bp1, bp2, *ans;
+errcode
+_byteptr8OPsimilar(CLUREF bp1, CLUREF bp2, CLUREF *ans)
 {
-WP *b1 = (WP*)bp1.ref;
-WP *b2 = (WP*)bp2.ref;
+    WP *b1 = (WP *)bp1.ref;
+    WP *b2 = (WP *)bp2.ref;
 
-	if (b1->buf.num == b2->buf.num &&
-	    b1->idx.num == b2->idx.num &&
-	    b1->max.num == b2->max.num)
-		  ans->tf = true;
-	else ans->tf = false;
-	signal(ERR_ok);
-	}
+    if (b1->buf.num == b2->buf.num
+	&& b1->idx.num == b2->idx.num
+	&& b1->max.num == b2->max.num)
+	ans->tf = true;
+    else
+	ans->tf = false;
+    signal(ERR_ok);
+}
 
-errcode _byteptr8OPcopy(bp, ans)
-CLUREF bp, *ans;
+
+errcode
+_byteptr8OPcopy(CLUREF bp, CLUREF *ans)
 {
-WP *b = (WP*)bp.ref;
-WP *temp;
-CLUREF t;
+    WP *b = (WP *)bp.ref;
+    WP *temp;
+    CLUREF t;
 
-	
-	clu_alloc(sizeof(WP), &temp);
-	temp->typ = b->typ;
-	temp->buf = b->buf;
-	temp->idx.num = b->idx.num;
-	temp->max.num = b->max.num;
-	t = *(CLUREF*)b;
-	ans->vec = t.vec;
-	signal(ERR_ok);
-	}
+    clu_alloc(sizeof(WP), &temp);
+    temp->typ = b->typ;
+    temp->buf = b->buf;
+    temp->idx.num = b->idx.num;
+    temp->max.num = b->max.num;
+    t = *(CLUREF*)b;
 
-errcode _byteptr8OP_gcd(bp, tab, ans)
-CLUREF bp, tab, *ans;
+    ans->vec = t.vec;
+    signal(ERR_ok);
+}
+
+
+errcode
+_byteptr8OP_gcd(CLUREF bp, CLUREF tab, CLUREF *ans)
 {
-errcode err;
-CLUREF temp_oneof, temp_seq, temp_proc1, temp_proc2, sz;
+    errcode err;
+    CLUREF ginfo;		// := ginfo$make_c_sel(...)
+    CLUREF sel_gproclist;	// := gproclist$[...]
+    CLUREF wordvec_gproc;	// := _wordvec$_gcd
+    CLUREF int_gproc;		// := int$_gcd
+    CLUREF sz;
 
-        proctypeOPnew(CLU_0, &temp_proc1);
-        temp_proc1.proc->proc = _wordvecOP_gcd;
-        temp_proc1.proc->type_owns = &_wordvec_own_init;
-        temp_proc1.proc->op_owns = &_wordvec_own_init;
+    proctypeOPnew(CLU_0, &wordvec_gproc);
+    wordvec_gproc.proc->proc = _wordvecOP_gcd;
+    wordvec_gproc.proc->type_owns = &_wordvec_own_init;
+    wordvec_gproc.proc->op_owns = &_wordvec_own_init;
 
-        proctypeOPnew(CLU_0, &temp_proc2);
-        temp_proc2.proc->proc = intOP_gcd;
-        temp_proc2.proc->type_owns = &int_own_init;
-        temp_proc2.proc->op_owns = &int_own_init;
+    proctypeOPnew(CLU_0, &int_gproc);
+    int_gproc.proc->proc = intOP_gcd;
+    int_gproc.proc->type_owns = &int_own_init;
+    int_gproc.proc->op_owns = &int_own_init;
 
-        sequenceOPnew2(CLU_3, &temp_seq);
-        temp_seq.vec->data[0] = temp_proc1.num;
-        temp_seq.vec->data[1] = temp_proc2.num;
-        temp_seq.vec->data[2] = temp_proc2.num;
+    sequenceOPnew2(CLU_3, &sel_gproclist);
+    sel_gproclist.vec->data[0] = wordvec_gproc.num;
+    sel_gproclist.vec->data[1] = int_gproc.num;
+    sel_gproclist.vec->data[2] = int_gproc.num;
 
-     	err = oneofOPnew(CLU_3, temp_seq, &temp_oneof);
-        if (err != ERR_ok) resignal(err);
+    err = oneofOPnew(CLU_3, sel_gproclist, &ginfo);
+    if (err != ERR_ok)
+	resignal(err);
 
-	sz.num = sizeof(WP);
-	err = gcd_tabOPinsert(tab, sz, temp_oneof, bp, ans);
-	if (err != ERR_ok) resignal(err);
-	signal(ERR_ok);
-	}
+    sz.num = sizeof(WP);
+
+    err = gcd_tabOPinsert(tab, sz, ginfo, bp, ans);
+    if (err != ERR_ok)
+	resignal(err);
+
+    signal(ERR_ok);
+}
+
 
 typedef struct {
-int count;
+    int count;
     OWNPTR type_owns;
     OWNPTR op_owns;
-struct OP_ENTRY entry[13];
+    struct OP_ENTRY entry[13];
 } _byteptr8_OPS;
-CLU_proc _byteptr8_oe_create = {{0,0,0,0}, _byteptr8OPcreate, 0};
-CLU_proc _byteptr8_oe_equal = {{0,0,0,0}, _byteptr8OPequal, 0};
-CLU_proc _byteptr8_oe_similar = {{0,0,0,0}, _byteptr8OPsimilar, 0};
-CLU_proc _byteptr8_oe_copy = {{0,0,0,0}, _byteptr8OPcopy, 0};
-CLU_proc _byteptr8_oe__gcd = {{0,0,0,0}, _byteptr8OP_gcd, 0};
-CLU_proc _byteptr8_oe_reset = {{0,0,0,0}, _byteptr8OPreset, 0};
-CLU_proc _byteptr8_oe_peek = {{0,0,0,0}, _byteptr8OPpeek, 0};
-CLU_proc _byteptr8_oe_deposit = {{0,0,0,0}, _byteptr8OPdeposit, 0};
-CLU_proc _byteptr8_oe_adjust = {{0,0,0,0}, _byteptr8OPadjust, 0};
-CLU_proc _byteptr8_oe_empty = {{0,0,0,0}, _byteptr8OPempty, 0};
-CLU_proc _byteptr8_oe_get_buf = {{0,0,0,0}, _byteptr8OPget_buf, 0};
-CLU_proc _byteptr8_oe_get_index = {{0,0,0,0}, _byteptr8OPget_index, 0};
-CLU_proc _byteptr8_oe_get_count = {{0,0,0,0}, _byteptr8OPget_count, 0};
 
-OWN_ptr _byteptr8_own_init = {1,0};
+static CLU_proc _byteptr8_oe_create = { .proc = _byteptr8OPcreate };
+static CLU_proc _byteptr8_oe_equal = { .proc = _byteptr8OPequal };
+static CLU_proc _byteptr8_oe_similar = { .proc = _byteptr8OPsimilar };
+static CLU_proc _byteptr8_oe_copy = { .proc = _byteptr8OPcopy };
+static CLU_proc _byteptr8_oe__gcd = { .proc = _byteptr8OP_gcd };
+static CLU_proc _byteptr8_oe_reset = { .proc = _byteptr8OPreset };
+static CLU_proc _byteptr8_oe_peek = { .proc = _byteptr8OPpeek };
+static CLU_proc _byteptr8_oe_deposit = { .proc = _byteptr8OPdeposit };
+static CLU_proc _byteptr8_oe_adjust = { .proc = _byteptr8OPadjust };
+static CLU_proc _byteptr8_oe_empty = { .proc = _byteptr8OPempty };
+static CLU_proc _byteptr8_oe_get_buf = { .proc = _byteptr8OPget_buf };
+static CLU_proc _byteptr8_oe_get_index = { .proc = _byteptr8OPget_index };
+static CLU_proc _byteptr8_oe_get_count = { .proc = _byteptr8OPget_count };
 
-_byteptr8_OPS _byteptr8_ops_actual = {13,
+OWN_ptr _byteptr8_own_init = { .init_flag = 1 };
+
+static _byteptr8_OPS _byteptr8_ops_actual = {
+    13,
     &_byteptr8_own_init,
-    &_byteptr8_own_init, {
-{&_byteptr8_oe_create, "create"},
-{&_byteptr8_oe_equal, "equal"},
-{&_byteptr8_oe_similar, "similar"},
-{&_byteptr8_oe_copy, "copy"},
-{&_byteptr8_oe__gcd, "_gcd"},
-{&_byteptr8_oe_reset, "reset"},
-{&_byteptr8_oe_peek, "peek"},
-{&_byteptr8_oe_deposit, "deposit"},
-{&_byteptr8_oe_adjust, "adjust"},
-{&_byteptr8_oe_empty, "empty"},
-{&_byteptr8_oe_get_buf, "get_buf"},
-{&_byteptr8_oe_get_index, "get_index"},
-{&_byteptr8_oe_get_count, "get_count"}}
+    &_byteptr8_own_init,
+    {
+	{ &_byteptr8_oe_create, "create" },
+	{ &_byteptr8_oe_equal, "equal" },
+	{ &_byteptr8_oe_similar, "similar" },
+	{ &_byteptr8_oe_copy, "copy" },
+	{ &_byteptr8_oe__gcd, "_gcd" },
+	{ &_byteptr8_oe_reset, "reset" },
+	{ &_byteptr8_oe_peek, "peek" },
+	{ &_byteptr8_oe_deposit, "deposit" },
+	{ &_byteptr8_oe_adjust, "adjust" },
+	{ &_byteptr8_oe_empty, "empty" },
+	{ &_byteptr8_oe_get_buf, "get_buf" },
+	{ &_byteptr8_oe_get_index, "get_index" },
+	{ &_byteptr8_oe_get_count, "get_count" },
+    }
 };
 
-_byteptr8_OPS *_byteptr8_ops = &_byteptr8_ops_actual;
+struct OPS *_byteptr8_ops = (struct OPS *)&_byteptr8_ops_actual;
