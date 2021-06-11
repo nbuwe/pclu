cbf0a60001c35021fc0ac719c3f560128d365491
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-11 18:32:30 +0300

    sysasm: _event.c - ANSIfy, prettify, comment.

--- code/sysasm/Opt/_event.c
+++ code/sysasm/Opt/_event.c
@@ -1,18 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _event.c,v 1.2 91/06/06 13:25:03 root Exp $";
-#endif
-/* $Log:	_event.c,v $
- * Revision 1.2  91/06/06  13:25:03  root
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  15:49:34  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -25,18 +11,26 @@ static char rcsid[] = "$Header: _event.c,v 1.2 91/06/06 13:25:03 root Exp $";
 
 int defer;
 
-errcode _eventOPdefer()
+
+/*
+ * Defer quit interrupt, stop array-trimming during G/C.
+ * Must be paired with undefer() call!
+ */
+errcode
+_eventOPdefer(void)
 {
-	defer += 1;
-	signal(ERR_ok);
-	}
+    ++defer;
+    signal(ERR_ok);
+}
 
 
-errcode _eventOPundefer()
+/*
+ * Allow quit interrupt, allow array-trimming during G/C.
+ * Must be paired with defer call!
+ */
+errcode
+_eventOPundefer(void)
 {
-	defer -= 1;
-	signal(ERR_ok);
-	}
-
-
-
+    --defer;
+    signal(ERR_ok);
+}
