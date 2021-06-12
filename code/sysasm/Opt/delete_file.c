a6b137de8d37df6e3e21f6cc0218e62129bf008b
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-12 17:57:33 +0300

    sysasm: delete_file.c - ANSIfy, prettify.  Same object code is generated.

--- code/sysasm/Opt/delete_file.c
+++ code/sysasm/Opt/delete_file.c
@@ -1,52 +1,37 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: delete_file.c,v 1.3 91/06/06 13:27:16 root Exp $";
-#endif
-/* $Log:	delete_file.c,v $
- * Revision 1.3  91/06/06  13:27:16  root
- * added copyright notice
- * 
- * Revision 1.2  91/05/31  12:16:39  root
- * removed unused variable
- * 
- * Revision 1.1  91/02/04  15:49:42  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*		IMPLEMENTATION OF		*/
 /*			delete_file		*/
 /*						*/
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <errno.h>
-
 #include "pclu_err.h"
 #include "pclu_sys.h"
-extern CLUREF empty_string;
+
+#include <errno.h>
+#include <unistd.h>
+
+extern errcode file_name_fill(CLUREF fn, CLUREF dsuffix, CLUREF *ret_1);
+extern errcode file_nameOPunparse(CLUREF x, CLUREF *ret_1);
 
 
-errcode delete_file(fn)
-CLUREF fn;
+errcode
+delete_file(CLUREF fn)
 {
+    int uerr;
+    errcode err;
+    CLUREF newfn, name;
 
-int uerr;
-errcode err;
-CLUREF newfn, name;
+    err = file_name_fill(fn, CLU_empty_string, &newfn);
+    if (err != ERR_ok) resignal(err);
 
-	err = file_name_fill(fn, empty_string, &newfn);
-	if (err != ERR_ok) resignal(err);
-	err = file_nameOPunparse(newfn, &name);
-	if (err != ERR_ok) resignal(err);
-	uerr = unlink(name.vec->data);
-	if (uerr != 0) {
-		elist[0] = _unix_erstr(errno);
-		signal(ERR_not_possible);
-		}
-	signal(ERR_ok);
-	}
+    err = file_nameOPunparse(newfn, &name);
+    if (err != ERR_ok) resignal(err);
 
+    uerr = unlink(name.str->data);
+    if (uerr != 0) {
+	elist[0] = _unix_erstr(errno);
+	signal(ERR_not_possible);
+    }
+    signal(ERR_ok);
+}
