da9f1a59e672babceb491923140d7c8e8874f014
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-12 16:38:46 +0300

    sysasm: _gprof.c - ANSIfy, prettify.

--- code/sysasm/Opt/_gprof.c
+++ code/sysasm/Opt/_gprof.c
@@ -1,18 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _gprof.c,v 1.2 91/06/06 13:25:32 root Exp $";
-#endif
-/* $Log:	_gprof.c,v $
- * Revision 1.2  91/06/06  13:25:32  root
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  15:49:36  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -23,20 +9,25 @@ static char rcsid[] = "$Header: _gprof.c,v 1.2 91/06/06 13:25:32 root Exp $";
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-errcode _gprofOPinit()
-{
-	signal(ERR_ok);
-	}
 
-errcode _gprofOPdump(fs)
-CLUREF fs;
+errcode
+_gprofOPinit(void)
 {
-	signal(ERR_ok);
-	}
+    signal(ERR_ok);
+}
 
-errcode _gprofOPcontrol(flag)
-CLUREF flag;
+
+errcode
+_gprofOPdump(CLUREF fs)
 {
-	signal(ERR_ok);
-	}
+    (void) fs;
+    signal(ERR_ok);
+}
 
+
+errcode
+_gprofOPcontrol(CLUREF flag)
+{
+    (void) flag;
+    signal(ERR_ok);
+}
