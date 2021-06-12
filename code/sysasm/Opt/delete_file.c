5348ea877993c58ebe6c48d3adab9478f467b6f4
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-12 18:00:01 +0300

    sysasm: delete_file.c - Prettify some more.  Same object code is generated.

--- code/sysasm/Opt/delete_file.c
+++ code/sysasm/Opt/delete_file.c
@@ -18,18 +18,21 @@ extern errcode file_nameOPunparse(CLUREF x, CLUREF *ret_1);
 errcode
 delete_file(CLUREF fn)
 {
-    int uerr;
     errcode err;
-    CLUREF newfn, name;
+    int status;
 
+    CLUREF newfn;
     err = file_name_fill(fn, CLU_empty_string, &newfn);
-    if (err != ERR_ok) resignal(err);
+    if (err != ERR_ok)
+	resignal(err);
 
-    err = file_nameOPunparse(newfn, &name);
-    if (err != ERR_ok) resignal(err);
+    CLUREF namestr;
+    err = file_nameOPunparse(newfn, &namestr);
+    if (err != ERR_ok)
+	resignal(err);
 
-    uerr = unlink(name.str->data);
-    if (uerr != 0) {
+    status = unlink(namestr.str->data);
+    if (status != 0) {
 	elist[0] = _unix_erstr(errno);
 	signal(ERR_not_possible);
     }
