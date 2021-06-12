ee1c66599891e86b9fa5c6d1f9e89de29bc43100
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-12 17:18:50 +0300

    sysasm: unix_cmd.c - ANSIfy, prettify.  Same object code is generated.

--- code/sysasm/Opt/unix_cmd.c
+++ code/sysasm/Opt/unix_cmd.c
@@ -1,3 +1,5 @@
+/* Copyright Massachusetts Institute of Technology 1990,1991 */
+
 /*						*/
 /*						*/
 /*		IMPLEMENTATION OF		*/
@@ -6,30 +8,36 @@
 
 #include <sys/wait.h>
 #undef signal
+
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-#ifndef LINUX
-extern char *system();
-#endif
+#include <stdlib.h>
 
-errcode unix_cmd(input)
-CLUREF input;
+extern errcode intOPunparse(CLUREF i, CLUREF *ans);
+extern errcode stringOPconcat(CLUREF s1, CLUREF s2, CLUREF *ans);
+extern errcode stringOPcons(const char *buf, CLUREF start, CLUREF len, CLUREF *ans);
+
+
+errcode
+unix_cmd(CLUREF input)
 {
-/* 2/26/01 dwc: unix prefers that system return int... */
-int result;
-CLUREF temp_str, n, num, size;
-errcode err;
+    int result;
+    CLUREF temp_str, n, num, size;
+    errcode err;
 
-	result = system((char*)input.vec->data); 
-/*	printf("err = %X, %X %X %X\n", err, WTERMSIG(err), 
-		WEXITSTATUS(err), WSTOPSIG(err)); */
-	if (result == 0) signal(ERR_ok);
-	err = stringOPcons("call to system failed: ", CLU_1, CLU_23, 
-			   &temp_str);
-	n.num = (long)result;
-	err = intOPunparse(n, &num);
-	err = stringOPconcat(temp_str, num, &temp_str);
-	elist[0] = temp_str;
-	signal(ERR_failure);
-	}
+    result = system((char *)input.vec->data);
+#if 0
+    printf("err = %X, %X %X %X\n", result, WTERMSIG(result),
+	   WEXITSTATUS(result), WSTOPSIG(result));
+#endif
+    if (result == 0)
+	signal(ERR_ok);
+
+    err = stringOPcons("call to system failed: ", CLU_1, CLU_23, &temp_str);
+    n.num = (long)result;
+    err = intOPunparse(n, &num);
+    err = stringOPconcat(temp_str, num, &temp_str);
+    elist[0] = temp_str;
+    signal(ERR_failure);
+}
