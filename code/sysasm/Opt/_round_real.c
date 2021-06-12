6e3dbfcf95e327cf9c77157e66ededb468fe8225
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-12 16:58:01 +0300

    sysasm: _round_real.c - Reduce scopes of local variables.

--- code/sysasm/Opt/_round_real.c
+++ code/sysasm/Opt/_round_real.c
@@ -13,15 +13,12 @@
 errcode
 _round_real(CLUREF digs, CLUREF num, CLUREF *ans1, CLUREF *ans2)
 {
-    int nz, i;
-    bool done;
-
     if (num.num > 9) num.num = 9;
     if (num.num <= 0) num.num = 1;
-    done = false;
 
     if (digs.str->data[num.num] >= '5') {
-	for (i = num.num - 1; i >= 0; --i) {
+	bool done = false;
+	for (int i = num.num - 1; i >= 0; --i) {
 	    if (digs.str->data[i] == '9') {
 		digs.str->data[i] = '0';
 	    }
@@ -31,6 +28,7 @@ _round_real(CLUREF digs, CLUREF num, CLUREF *ans1, CLUREF *ans2)
 		break;
 	    }
 	}
+	/* sic.  it's probably not the best variable name... */
 	if (done == false) {
 	    digs.str->data[0] = '1';
 	    ans1->num = 1;
@@ -39,8 +37,8 @@ _round_real(CLUREF digs, CLUREF num, CLUREF *ans1, CLUREF *ans2)
 	}
     }
 
-    nz = num.num;
-    for (i = num.num - 1; i >= 0; --i) {
+    int nz = num.num;
+    for (int i = num.num - 1; i >= 0; --i) {
 	if (digs.str->data[i] != '0')
 	    break;
 	nz -= 1;
