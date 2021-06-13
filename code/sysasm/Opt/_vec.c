3ba807dab2a22caddc5054f37f96aaac591dc115
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-14 01:39:45 +0300

    sysasm: _vec.c - ANSIfy, prettify.  Same object code is generated.

--- code/sysasm/Opt/_vec.c
+++ code/sysasm/Opt/_vec.c
@@ -1,31 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _vec.c,v 1.6 91/06/06 13:26:15 root Exp $";
-#endif
-/* $Log:	_vec.c,v $
- * Revision 1.6  91/06/06  13:26:15  root
- * added copyright notice
- * 
- * Revision 1.5  91/06/03  12:15:43  root
- * sparcestation compatibility: int->CLUREF
- * 
- * Revision 1.4  91/05/31  11:58:33  root
- * fixed aggregate initialization in ops_actual
- * changed int arguement to CLUREF argument in copy operation
- * 
- * Revision 1.3  91/02/14  16:37:44  dcurtis
- * fixed move_rl, move_lr off by one in index in copy
- * 
- * Revision 1.2  91/02/14  16:15:39  dcurtis
- * fixed move_rl, move_lr to signal ok if length = 0
- * fixed move_rl, move_lr to copy between vec args, rather than size args
- * 
- * Revision 1.1  91/02/04  15:49:38  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*						*/
@@ -37,216 +10,296 @@ static char rcsid[] = "$Header: _vec.c,v 1.6 91/06/06 13:26:15 root Exp $";
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
+extern errcode gcd_tabOPinsert(CLUREF tab, CLUREF z, CLUREF inf, CLUREF x, CLUREF *ans);
+extern errcode sequenceOPinternal_print(CLUREF s, CLUREF pst, CLUREF pfcn);
+
+
+/*
+ * What _vec[T] requires of T.
+ */
+static struct /* REQS */ {
+    int count;
+    struct REQ_ENTRY entry[2];
+} _vec_of_t_reqs_actual = {
+    2, {
+	{ "_gcd" },
+	{ "debug_print" },
+    }
+};
+
+const struct REQS * const _vec_of_t_reqs
+    = (const struct REQS *)&_vec_of_t_reqs_actual;
+
+
+/*
+ * An interface to T for a _vec[T] instance (as specified by REQS).
+ */
 typedef struct {
-int count;
-struct REQ_ENTRY entry[2];
-} _vec_of_t_REQS;
-
-typedef struct {
-int count;
-OWNPTR type_owns;
-OWNPTR op_owns;
-struct OP_ENTRY _gcd;
-struct OP_ENTRY debug_print;
-} _vec_of_t_TABLE;
-
-_vec_of_t_REQS _vec_of_t_reqs_actual = {2, "_gcd", "debug_print"};
-_vec_of_t_REQS *_vec_of_t_reqs = &_vec_of_t_reqs_actual;
+    int count;
+    OWNPTR type_owns;
+    OWNPTR op_owns;
+    struct OP_ENTRY _gcd;
+    struct OP_ENTRY debug_print;
+} _vec_of_t_OPS;
 
 
-errcode _vecOPcreate(sz, ans)
-CLUREF sz, *ans;
+/*
+ * Own data of a _vec[T] instance.
+ */
+typedef struct /* OWN_ptr */ {
+    long _vec_own_init;		/* unused */
+    const _vec_of_t_OPS * const t_ops;
+} _vec_OWN_DEFN;
+
+
+/*
+ * Tell instantiation code how to construct an _OWN_DEFN object for an
+ * instantce.  What "own_count" really tells is where the parameters
+ * start (OPS for types, values for constants).
+ */
+const OWN_req _vec_ownreqs = { sizeof(_vec_OWN_DEFN), 1 };
+
+
+
+errcode
+_vecOPcreate(CLUREF sz, CLUREF *ans)
 {
-errcode err;
-CLUREF temp;
-int i;
-	if (sz.num < 0) signal(ERR_negative_size);
+    errcode err;
+    CLUREF temp;
+    int i;
 
-/* Using sequenceOPnew for empty _vec's is NOT A GOOD IDEA:
-	sequences are immutable, but _vec's are used
-	for array stores by gc_read and there is but a single empty
-	sequence in the world and mtv's array$addh frees stores that
-	are being replaced.  7/23/93 dcurtis
+    CLU_NOREF(err);
 
-	if (sz.num == 0) {
-		err = sequenceOPnew(&temp);
-		ans->vec = temp.vec;
-		signal(ERR_ok);
-		}
-*/
-	if (sz.num > MAX_SEQ) signal(ERR_toobig);
+    if (sz.num < 0)
+	signal(ERR_negative_size);
 
-	clu_alloc((sz.num-1)*sizeof(CLUREF) + sizeof(CLU_sequence), &temp);
-	temp.vec->size = sz.num;
-	temp.vec->typ.val = CT_AGG;
-	temp.vec->typ.mark = 0;
-	temp.vec->typ.refp = 0;
-
-	for (i = 0; i < sz.num; i++) {
-		temp.vec->data[i] = 0;
-		}
+    /*
+     * Using sequenceOPnew for empty _vec's is NOT A GOOD IDEA:
+     * sequences are immutable, but _vec's are used for array stores
+     * by gc_read and there is but a single empty sequence in the
+     * world and mtv's array$addh frees stores that are being
+     * replaced.  7/23/93 dcurtis
+     */
+#if 0
+    if (sz.num == 0) {
+	err = sequenceOPnew(&temp);
 	ans->vec = temp.vec;
 	signal(ERR_ok);
-	}
+    }
+#endif
 
-errcode _vecOPcopy(v, ans)
-CLUREF v, *ans;
+    if (sz.num > MAX_SEQ)
+	signal(ERR_toobig);
+
+    clu_alloc(sizeof(CLU_sequence) + (sz.num - 1) * CLUREFSZ,
+	      &temp);
+    temp.vec->size = sz.num;
+    temp.vec->typ.val = CT_AGG;
+    temp.vec->typ.mark = 0;
+    temp.vec->typ.refp = 0;
+
+    for (i = 0; i < sz.num; ++i) {
+	temp.vec->data[i] = 0;
+    }
+
+    ans->vec = temp.vec;
+    signal(ERR_ok);
+}
+
+
+errcode
+_vecOPcopy(CLUREF v, CLUREF *ans)
 {
-errcode err;
-CLUREF temp, sz;
-int i;
+    errcode err;
+    CLUREF temp, sz;
+    int i;
 
-	sz.num = v.vec->size;
-	err = _vecOPcreate(sz, &temp);
-	if (err != ERR_ok) resignal(err);
-	for (i = 0; i < v.vec->size; i++) {
-		temp.vec->data[i] = v.vec->data[i];
-		}
-	ans->vec = temp.vec;
-	signal(ERR_ok);
-	}
+    sz.num = v.vec->size;
+    err = _vecOPcreate(sz, &temp);
+    if (err != ERR_ok)
+	resignal(err);
 
-errcode _vecOPfetch(v, i, ans)
-CLUREF v, i, *ans;
+    for (i = 0; i < v.vec->size; i++) {
+	temp.vec->data[i] = v.vec->data[i];
+    }
+
+    ans->vec = temp.vec;
+    signal(ERR_ok);
+}
+
+
+errcode
+_vecOPfetch(CLUREF v, CLUREF i, CLUREF *ans)
 {
-	if (i.num < 1) signal(ERR_bounds);
-	if (i.num > v.vec->size) signal(ERR_bounds);
-	ans->num = v.vec->data[i.num-1];
-	signal(ERR_ok);
-	}
+    if (i.num < 1)
+	signal(ERR_bounds);
+    if (i.num > v.vec->size)
+	signal(ERR_bounds);
 
-errcode _vecOPstore(v, i, val)
-CLUREF v, i, val;
+    ans->num = v.vec->data[i.num - 1];
+    signal(ERR_ok);
+}
+
+
+errcode
+_vecOPstore(CLUREF v, CLUREF i, CLUREF val)
 {
-	if (i.num < 1) signal(ERR_bounds);
-	if (i.num > v.vec->size) signal(ERR_bounds);
-	v.vec->data[i.num-1] = val.num;
-	signal(ERR_ok);
-	}
+    if (i.num < 1)
+	signal(ERR_bounds);
+    if (i.num > v.vec->size)
+	signal(ERR_bounds);
 
-errcode _vecOPsize(v, ans)
-CLUREF v, *ans;
+    v.vec->data[i.num-1] = val.num;
+    signal(ERR_ok);
+}
+
+
+errcode
+_vecOPsize(CLUREF v, CLUREF *ans)
 {
-	ans->num = v.vec->size;
-	signal(ERR_ok);
-	}
+    ans->num = v.vec->size;
+    signal(ERR_ok);
+}
 
-errcode _vecOPequal(v1, v2, ans)
-CLUREF v1, v2, *ans;
+
+errcode
+_vecOPequal(CLUREF v1, CLUREF v2, CLUREF *ans)
 {
-	if (v1.ref == v2.ref) ans->tf = true;
-	else ans->tf = false;
-	signal(ERR_ok);
-	}
+    if (v1.ref == v2.ref)
+	ans->tf = true;
+    else
+	ans->tf = false;
+    signal(ERR_ok);
+}
 
-errcode _vecOPmove_lr(v1, s1, v2, s2, len)
-CLUREF v1, s1, v2, s2, len;
+
+errcode
+_vecOPmove_lr(CLUREF v1, CLUREF s1, CLUREF v2, CLUREF s2, CLUREF len)
 {
-int copycount, i;
-
-	if (len.num < 0) signal(ERR_negative_size);
-	if (len.num == 0) signal(ERR_ok);
-	if (s1.num > v1.vec->size) signal(ERR_bounds);
-	if (s2.num > v2.vec->size) signal(ERR_bounds);
-	copycount = len.num;
-	if (s1.num + copycount > v1.vec->size) 
-		copycount = v1.vec->size - s1.num + 1;
-	if (s2.num + copycount > v2.vec->size) 
-		copycount = v2.vec->size - s2.num + 1;
-	for (i = 0; i < copycount; i++) {
-		v2.vec->data[s2.num + i - 1] = v1.vec->data[s1.num + i - 1];
-		}
+    if (len.num < 0)
+	signal(ERR_negative_size);
+    if (len.num == 0)
 	signal(ERR_ok);
-	}
+    if (s1.num > v1.vec->size)
+	signal(ERR_bounds);
+    if (s2.num > v2.vec->size)
+	signal(ERR_bounds);
 
-errcode _vecOPmove_rl(v1, s1, v2, s2, len)
-CLUREF v1, s1, v2, s2, len;
+    int copycount = len.num;
+    if (s1.num + copycount > v1.vec->size)
+	copycount = v1.vec->size - s1.num + 1;
+    if (s2.num + copycount > v2.vec->size)
+	copycount = v2.vec->size - s2.num + 1;
+    for (int i = 0; i < copycount; ++i) {
+	v2.vec->data[s2.num + i - 1] = v1.vec->data[s1.num + i - 1];
+    }
+    signal(ERR_ok);
+}
+
+
+errcode
+_vecOPmove_rl(CLUREF v1, CLUREF s1, CLUREF v2, CLUREF s2, CLUREF len)
 {
-int copycount, i;
-
-	if (len.num < 0) signal(ERR_negative_size);
-	if (len.num == 0) signal(ERR_ok);
-	if (s1.num > v1.vec->size) signal(ERR_bounds);
-	if (s2.num > v2.vec->size) signal(ERR_bounds);
-	copycount = len.num;
-	if (s1.num + copycount > v1.vec->size) 
-		copycount = v1.vec->size - s1.num + 1;
-	if (s2.num + copycount > v2.vec->size) 
-		copycount = v2.vec->size - s2.num + 1;
-	for (i = copycount - 1; i >= 0; i--) {
-		v2.vec->data[s2.num + i - 1] = v1.vec->data[s1.num + i - 1];
-		}
+    if (len.num < 0)
+	signal(ERR_negative_size);
+    if (len.num == 0)
 	signal(ERR_ok);
-	}
+    if (s1.num > v1.vec->size)
+	signal(ERR_bounds);
+    if (s2.num > v2.vec->size)
+	signal(ERR_bounds);
 
-errcode _vecOP_gcd(v, tab, ans)
-CLUREF v, tab, *ans;
+    int copycount = len.num;
+    if (s1.num + copycount > v1.vec->size)
+	copycount = v1.vec->size - s1.num + 1;
+    if (s2.num + copycount > v2.vec->size)
+	copycount = v2.vec->size - s2.num + 1;
+    for (int i = copycount - 1; i >= 0; --i) {
+	v2.vec->data[s2.num + i - 1] = v1.vec->data[s1.num + i - 1];
+    }
+    signal(ERR_ok);
+}
+
+
+errcode
+_vecOP_gcd(CLUREF v, CLUREF tab, CLUREF *ans)
 {
-_vec_of_t_TABLE *table = (_vec_of_t_TABLE*)CUR_PROC_VAR.proc->type_owns->info[0]; /* ptr to _gcd fcns */
-errcode err;
-CLUREF temp_oneof, sz, fcn;
+    _vec_of_t_OPS *table
+	= (_vec_of_t_OPS*)CUR_PROC_VAR.proc->type_owns->info[0];
+    errcode err;
+    CLUREF temp_oneof, sz, fcn;
 
-	fcn.proc = table->_gcd.fcn;
-	err = oneofOPnew(CLU_2, fcn, &temp_oneof);
-	if (err != ERR_ok) resignal(err);
-	sz.num = v.vec->size * GCD_REF_SIZE + 8;
-	err = gcd_tabOPinsert(tab, sz, temp_oneof, v, ans);
-	if (err != ERR_ok) resignal(err);
-	signal(ERR_ok);
-	}
+    fcn.proc = table->_gcd.fcn;
+    err = oneofOPnew(CLU_2, fcn, &temp_oneof);
+    if (err != ERR_ok)
+	resignal(err);
 
-errcode _vecOPdebug_print(v, pst)
-CLUREF v, pst;
+    sz.num = v.vec->size * GCD_REF_SIZE + 8;
+    err = gcd_tabOPinsert(tab, sz, temp_oneof, v, ans);
+    if (err != ERR_ok)
+	resignal(err);
+    signal(ERR_ok);
+}
+
+
+errcode
+_vecOPdebug_print(CLUREF v, CLUREF pst)
 {
-errcode err;
-_vec_of_t_TABLE *table = (_vec_of_t_TABLE*)CUR_PROC_VAR.proc->type_owns->info[0];
-CLUREF pfcn;
+    errcode err;
+    _vec_of_t_OPS *table
+	= (_vec_of_t_OPS *)CUR_PROC_VAR.proc->type_owns->info[0];
 
-	pfcn.proc = table->debug_print.fcn;
-	err = sequenceOPinternal_print(v, pst, pfcn);
-	if (err != ERR_ok) resignal(err);
-	signal(ERR_ok);
-	}
+    CLUREF pfcn;
+    pfcn.proc = table->debug_print.fcn;
+    err = sequenceOPinternal_print(v, pst, pfcn);
+    if (err != ERR_ok)
+	resignal(err);
+    signal(ERR_ok);
+}
 
 
-const OWN_req _vec_ownreqs = { 2 * UNIT, 1 };
 
-typedef struct{
+OWN_ptr _vec_own_init;		/* dummy */
+
+#define CLU_proc_INIT(f) {			\
+ /* .typ = { .val = CT_PROC }, */		\
+    .proc = f,					\
+    .type_owns = &_vec_own_init,		\
+    .op_owns = &_vec_own_init,			\
+}
+
+static CLU_proc _vec_oe__gcd = CLU_proc_INIT(_vecOP_gcd);
+static CLU_proc _vec_oe_debug_print = CLU_proc_INIT(_vecOPdebug_print);
+static CLU_proc _vec_oe_create = CLU_proc_INIT(_vecOPcreate);
+static CLU_proc _vec_oe_equal = CLU_proc_INIT(_vecOPequal);
+static CLU_proc _vec_oe_copy = CLU_proc_INIT(_vecOPcopy);
+static CLU_proc _vec_oe_fetch = CLU_proc_INIT(_vecOPfetch);
+static CLU_proc _vec_oe_store = CLU_proc_INIT(_vecOPstore);
+static CLU_proc _vec_oe_size = CLU_proc_INIT(_vecOPsize);
+static CLU_proc _vec_oe_move_lr = CLU_proc_INIT(_vecOPmove_lr);
+static CLU_proc _vec_oe_move_rl = CLU_proc_INIT(_vecOPmove_rl);
+
+static struct /* OPS */ {
     int count;
     OWNPTR type_owns;
     OWNPTR op_owns;
     struct OP_ENTRY entry[10];
-} _vec_OPS;
-
-OWN_ptr _vec_own_init = {0, 0};
-
-CLU_proc _vec_oe__gcd = {{0,0,0,0}, _vecOP_gcd, &_vec_own_init, &_vec_own_init};
-CLU_proc _vec_oe_debug_print = {{0,0,0,0}, _vecOPdebug_print, &_vec_own_init, &_vec_own_init};
-CLU_proc _vec_oe_create = {{0,0,0,0}, _vecOPcreate, &_vec_own_init, &_vec_own_init};
-CLU_proc _vec_oe_equal = {{0,0,0,0}, _vecOPequal, &_vec_own_init, &_vec_own_init};
-CLU_proc _vec_oe_copy = {{0,0,0,0}, _vecOPcopy, &_vec_own_init, &_vec_own_init};
-CLU_proc _vec_oe_fetch = {{0,0,0,0}, _vecOPfetch, &_vec_own_init, &_vec_own_init};
-CLU_proc _vec_oe_store = {{0,0,0,0}, _vecOPstore, &_vec_own_init, &_vec_own_init};
-CLU_proc _vec_oe_size = {{0,0,0,0}, _vecOPsize, &_vec_own_init, &_vec_own_init};
-CLU_proc _vec_oe_move_lr = {{0,0,0,0}, _vecOPmove_lr, &_vec_own_init, &_vec_own_init};
-CLU_proc _vec_oe_move_rl = {{0,0,0,0}, _vecOPmove_rl, &_vec_own_init, &_vec_own_init};
-
-
-_vec_OPS _vec_ops_actual = {10,
+} _vec_ops_actual = {
+    10,
     &_vec_own_init,
-    &_vec_own_init, {
-{&_vec_oe_create, "create"},
-{&_vec_oe_equal, "equal"},
-{&_vec_oe_copy, "copy"},
-{&_vec_oe_fetch, "fetch"},
-{&_vec_oe_store, "store"},
-{&_vec_oe_size, "size"},
-{&_vec_oe_move_lr, "move_lr"},
-{&_vec_oe_move_rl, "move_rl"},
-{&_vec_oe__gcd, "_gcd"},
-{&_vec_oe_debug_print, "debug_print"}}
+    &_vec_own_init,
+    {
+	{ &_vec_oe_create, "create" },
+	{ &_vec_oe_equal, "equal" },
+	{ &_vec_oe_copy, "copy" },
+	{ &_vec_oe_fetch, "fetch" },
+	{ &_vec_oe_store, "store" },
+	{ &_vec_oe_size, "size" },
+	{ &_vec_oe_move_lr, "move_lr" },
+	{ &_vec_oe_move_rl, "move_rl" },
+	{ &_vec_oe__gcd, "_gcd" },
+	{ &_vec_oe_debug_print, "debug_print" },
+    }
 };
 
-_vec_OPS *_vec_ops = (_vec_OPS *)&_vec_ops_actual;
-
-
+struct OPS *_vec_ops = (struct OPS *)&_vec_ops_actual;
