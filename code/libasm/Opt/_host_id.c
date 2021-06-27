5866e3a28298d5b5e2a7b612176a65a8196e6a6c
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 02:13:49 +0300

    libasm: _host_id - ANSIfy.

--- code/libasm/Opt/_host_id.c
+++ code/libasm/Opt/_host_id.c
@@ -1,18 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _host_id.c,v 1.2 91/06/06 13:47:44 dcurtis Exp $";
-#endif
-/* $Log:	_host_id.c,v $
- * Revision 1.2  91/06/06  13:47:44  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:20:39  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -22,15 +8,15 @@ static char rcsid[] = "$Header: _host_id.c,v 1.2 91/06/06 13:47:44 dcurtis Exp $
 
 #include "pclu_err.h"
 #include "pclu_sys.h"
-#include <sys/types.h>
 
-errcode _host_id(ans1, ans2)
-CLUREF *ans1, *ans2;
+#include <unistd.h>
+
+
+errcode
+_host_id(CLUREF *ans1, CLUREF *ans2)
 {
-long hostid;
-
-	hostid = gethostid();
-	ans1->num = hostid>>16;
-	ans2->num = hostid & 0xffff;
-	signal(ERR_ok);
-	}
+    long hostid = gethostid();
+    ans1->num = hostid >> 16;	/* XXX */
+    ans2->num = hostid & 0xffff;
+    signal(ERR_ok);
+}
