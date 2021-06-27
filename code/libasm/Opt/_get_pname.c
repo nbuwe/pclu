93439f718dc6926467db39778aa158d322542e46
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 02:43:34 +0300

    libasm: _get_pname, _get_xjname - ANSIfy, simplify, sync with reality.
    Get function descriptions from etc.spc.  Make _get_xjname() implement
    what it is documented to do (return only the basename).

--- code/libasm/Opt/_get_pname.c
+++ code/libasm/Opt/_get_pname.c
@@ -1,23 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _get_pname.c,v 1.4 91/06/06 13:46:01 dcurtis Exp $";
-#endif
-/* $Log:	_get_pname.c,v $
- * Revision 1.4  91/06/06  13:46:01  dcurtis
- * added copyright notice
- * 
- * Revision 1.3  91/06/03  16:53:11  root
- * sparcstation compatibility: int->CLUREF
- * 
- * Revision 1.2  91/02/11  11:45:54  dcurtis
- * changed bogus code to sane code
- * 
- * Revision 1.1  91/02/04  23:20:28  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*		IMPLEMENTATION OF		*/
@@ -27,23 +8,17 @@ static char rcsid[] = "$Header: _get_pname.c,v 1.4 91/06/06 13:46:01 dcurtis Exp
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
+/* sysasm: util.c */
 extern char **argv;
-#include <errno.h>
 
-errcode _get_pname(ans)
-CLUREF *ans;
+
+/*
+ * Return program name with directory, if any.
+ */
+errcode
+_get_pname(CLUREF *ans)
 {
-errcode err;
-CLUREF temp;
-char *buf, *end;
-CLUREF len;
-
-	len.num = strlen(argv[0]);
-	err = stringOPcons(argv[0], CLU_1, len, &temp);
-	if (err == -1) {
-		elist[0] = _unix_erstr(errno);
-		signal(ERR_not_possible);
-		}
-	ans->vec = temp.vec;
-	signal(ERR_ok);
-	}
+    size_t len = strlen(argv[0]);
+    stringOPcons(argv[0], CLU_1, CLUREF_make_num(len), ans);
+    signal(ERR_ok);
+}
