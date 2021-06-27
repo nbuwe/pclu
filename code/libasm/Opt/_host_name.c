5705d9045b6bb504483ca6c3849f9ad6559d6f25
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 02:09:44 +0300

    libasm: _host_name - ANSIfy, use MAXHOSTNAMELEN.

--- code/libasm/Opt/_host_name.c
+++ code/libasm/Opt/_host_name.c
@@ -1,56 +1,27 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
 
-#ifndef lint
-static char rcsid[] = "$Header: _host_name.c,v 1.3 91/06/06 13:47:55 dcurtis Exp $";
-#endif
-/* $Log:	_host_name.c,v $
- * Revision 1.3  91/06/06  13:47:55  dcurtis
- * added copyright notice
- * 
- * Revision 1.2  91/06/03  16:58:15  root
- * sparcstation compatibility: int->CLUREF
- * 
- * Revision 1.1  91/02/04  23:20:40  mtv
- * Initial revision
- * 
- */
-
-/*						*/
-/*		IMPLEMENTATION OF		*/
-/*			_host_name		*/
-/*						*/
-
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <sys/param.h>
 #include <errno.h>
+#include <unistd.h>
+#undef signal
 
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-extern CLUREF empty_string;
 
-/* per documentation of gethostname */
-#define BUFSIZE 31
-
-errcode _host_name(ans)
-CLUREF *ans;
+errcode
+_host_name(CLUREF *ans)
 {
+    int status;
 
-int uerr;
-errcode err;
-char buf[BUFSIZE];
-CLUREF temp, len;
+    char buf[MAXHOSTNAMELEN];
+    status = gethostname(buf, sizeof(buf));
+    if (status != 0) {
+	elist[0] = _unix_erstr(errno);
+	signal(ERR_not_possible);
+    }
 
-	uerr = gethostname(buf, BUFSIZE);
-	if (uerr != 0) {
-		elist[0] = _unix_erstr(errno);
-		signal(ERR_not_possible);
-		}
-	len.num = strlen(buf);
-	err = stringOPcons(buf, CLU_1, len, &temp);
-	if (err != ERR_ok) resignal(err);
-	ans->str = temp.str;
-	signal(ERR_ok);
-	}
-
+    size_t len = strlen(buf);
+    stringOPcons(buf, CLU_1, CLUREF_make_num(len), ans);
+    signal(ERR_ok);
+}
