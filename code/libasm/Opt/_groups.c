eb34348c3e701eb8e4fb4e8431724983f4f5660e
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 01:25:20 +0300

    libasm: _groups - ANSIfy, be more flexible.

--- code/libasm/Opt/_groups.c
+++ code/libasm/Opt/_groups.c
@@ -1,23 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _groups.c,v 1.4 91/06/06 13:47:07 dcurtis Exp $";
-#endif
-/* $Log:	_groups.c,v $
- * Revision 1.4  91/06/06  13:47:07  dcurtis
- * added copyright notice
- * 
- * Revision 1.3  91/06/05  13:24:05  root
- * added elist to sequence$new2
- * 
- * Revision 1.2  91/06/03  16:56:53  root
- * sparcstation compatibility: int->CLUREF
- * 
- * Revision 1.1  91/02/04  23:20:36  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*						*/
@@ -25,33 +6,69 @@ static char rcsid[] = "$Header: _groups.c,v 1.4 91/06/06 13:47:07 dcurtis Exp $"
 /*			_groups			*/
 /*						*/
 
+#include <sys/param.h>
+#include <errno.h>
+#include <unistd.h>
+#undef signal
+
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-#include <errno.h>
-extern CLUREF empty_string;
 
-errcode _groups(ans)
-CLUREF *ans;
+/*
+ * POSIX requires {NGROUPS_MAX} to be at least 8, NetBSD has 16, but
+ * Linux has 64K, so allocating that statically or dynamically is a
+ * bit of an overkill.
+ *
+ * {NGROUPS_MAX} is not necessarily a constant on all implementations.
+ */
+#define NGROUPS_STATIC 16
+
+
+errcode
+_groups(CLUREF *ans)
 {
-int maxcount;
-CLUREF count;
-CLUREF temp;
-int list[16];
-errcode err;
-int i;
+    int ngroups;
 
-	maxcount = 16;
-	count.num = getgroups(maxcount, list);
-		if (count.num == -1) {
-		elist[0] = _unix_erstr(errno);
-		signal(ERR_not_possible);
-		}
-	err = sequenceOPnew2(count, &temp);
-	if (err != ERR_ok) resignal(err);
-	for (i = 0; i < count.num; i++) {
-		temp.vec->data[i] = list[i];
-		}
-	ans->vec = temp.vec;
+    gid_t gidset_static[NGROUPS_STATIC];
+    int ngroups_max = NGROUPS_STATIC;
+    gid_t *gidset = gidset_static;
+
+    ngroups = getgroups(ngroups_max, gidset);
+    if (ngroups < 0) {
+	if (errno != EINVAL)
+	    goto ex_errno;
+
+	/*
+	 * EINVAL means the local fixed size array was too small, so
+	 * we need to go dynamic.  Ask the system how much we need
+	 * (and assume that that value won't change).
+	 */
+	ngroups_max = getgroups(0, gidset);
+	if (ngroups_max < 0)	/* very unlikely */
+	    goto ex_errno;
+
+	clu_alloc_atomic(ngroups_max * sizeof(gid_t), &gidset);
+	ngroups = getgroups(ngroups_max, gidset);
+	if (ngroups < 0)
+	    goto ex_errno;
+    }
+
+    if (ngroups == 0) {		/* should not happen */
+	sequenceOPnew(ans);
 	signal(ERR_ok);
-	}
+    }
+
+    CLUREF groups;
+    sequenceOPnew2(CLUREF_make_num(ngroups), &groups);
+    for (int i = 0; i < ngroups; ++i)
+	groups.vec->data[i] = gidset[i];
+
+    *ans = groups;
+    signal(ERR_ok);
+
+  ex_errno: {
+	elist[0] = _unix_erstr(errno);
+	signal(ERR_not_possible);
+    }
+}
