6b6ad896c478d6aa3787132e215264b8a5d6fe43
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-27 03:40:25 +0300

    libasm: link_file.c - ANSIfy, prettify.  Same object code is generated.

--- code/libasm/Opt/link_file.c
+++ code/libasm/Opt/link_file.c
@@ -1,17 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: link_file.c,v 1.2 91/06/06 13:54:48 dcurtis Exp $";
-#endif
-/* $Log:	link_file.c,v $
- * Revision 1.2  91/06/06  13:54:48  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:21:31  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*		IMPLEMENTATION OF		*/
@@ -22,32 +9,40 @@ static char rcsid[] = "$Header: link_file.c,v 1.2 91/06/06 13:54:48 dcurtis Exp 
 #include "pclu_sys.h"
 
 #include <errno.h>
-extern CLUREF empty_string;
+#include <unistd.h>
 
-errcode link_file(ofn, nfn)
-CLUREF ofn, nfn;
+errcode file_nameOPunparse(CLUREF x, CLUREF *ret_1);
+errcode file_name_fill(CLUREF fn, CLUREF dsuffix, CLUREF *ret_1);
+
+
+errcode
+link_file(CLUREF ofn, CLUREF nfn)
 {
-errcode err;
-int uerr;
-CLUREF wd, oname, nname, home;
+    errcode err;
+    int status;
 
-	err = file_name_fill(ofn, empty_string, &oname);
-	if (err != ERR_ok) resignal(err);
+    CLUREF oname;
+    err = file_name_fill(ofn, CLU_empty_string, &oname);
+    if (err != ERR_ok)
+	resignal(err);
 
-	err = file_nameOPunparse(oname, &oname);
-	if (err != ERR_ok) resignal(err);
+    err = file_nameOPunparse(oname, &oname);
+    if (err != ERR_ok)
+	resignal(err);
 
-	err = file_name_fill(nfn, empty_string, &nname);
-	if (err != ERR_ok) resignal(err);
+    CLUREF nname;
+    err = file_name_fill(nfn, CLU_empty_string, &nname);
+    if (err != ERR_ok)
+	resignal(err);
 
-	err = file_nameOPunparse(nname, &nname);
-	if (err != ERR_ok) resignal(err);
+    err = file_nameOPunparse(nname, &nname);
+    if (err != ERR_ok)
+	resignal(err);
 
-	uerr = link(oname.ref, nname.ref);
-	if (uerr == 0) {
-		signal(ERR_ok);
-		}
-	elist[0] = _unix_erstr(errno);
-	signal(ERR_not_possible);
-	}
-
+    status = link(oname.ref, nname.ref);
+    if (status == 0) {
+	signal(ERR_ok);
+    }
+    elist[0] = _unix_erstr(errno);
+    signal(ERR_not_possible);
+}
