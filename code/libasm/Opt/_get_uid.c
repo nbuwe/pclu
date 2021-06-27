544f59ef51e8286ff599a4f4c784410971c983a5
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-27 18:36:48 +0300

    libasm: _get_uid - ANSIfy.  Fix to match reality.
    For reference: glibc notes in the HISTORY section of getuid(2) that
    "in UNIX V6 the getuid() call returned (euid << 8) + uid."  This seems
    the most likely explanation of the original code, but the original
    code did >>16, not >>8.  Water under the bridge, anyway.

--- code/libasm/Opt/_get_uid.c
+++ code/libasm/Opt/_get_uid.c
@@ -1,18 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _get_uid.c,v 1.2 91/06/06 13:46:36 dcurtis Exp $";
-#endif
-/* $Log:	_get_uid.c,v $
- * Revision 1.2  91/06/06  13:46:36  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:20:32  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -22,15 +8,17 @@ static char rcsid[] = "$Header: _get_uid.c,v 1.2 91/06/06 13:46:36 dcurtis Exp $
 
 #include "pclu_err.h"
 #include "pclu_sys.h"
-#include <sys/types.h>
 
-errcode _get_uid(ans1, ans2)
-CLUREF *ans1, *ans2;
+#include <unistd.h>
+
+
+/*
+ * Returns real user ID, effective user ID.
+ */
+errcode
+_get_uid(CLUREF *uid, CLUREF *euid)
 {
-uid_t uid;
-
-	uid = getuid();
-	ans1->num = uid>>16;
-	ans2->num = uid & 0xffff;
-	signal(ERR_ok);
-	}
+    uid->num = getuid();
+    euid->num = geteuid();
+    signal(ERR_ok);
+}
