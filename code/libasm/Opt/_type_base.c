df8a770bb5c6db938650f56da12daf36d60068aa
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 01:29:26 +0300

    libasm: _type_base - ANSIfy, add comment.

--- code/libasm/Opt/_type_base.c
+++ code/libasm/Opt/_type_base.c
@@ -1,17 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _type_base.c,v 1.2 91/06/06 13:52:04 dcurtis Exp $";
-#endif
-/* $Log:	_type_base.c,v $
- * Revision 1.2  91/06/06  13:52:04  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:21:06  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*						*/
@@ -21,9 +8,15 @@ static char rcsid[] = "$Header: _type_base.c,v 1.2 91/06/06 13:52:04 dcurtis Exp
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-errcode _type_base(ans)
-CLUREF *ans;
+
+/*
+ * Returns base of type ids.
+ *
+ * uwe: Probably obsolete.
+ */
+errcode
+_type_base(CLUREF *ans)
 {
-	ans->num = 020000;
-	signal(ERR_ok);
-	}
+    ans->num = 020000;
+    signal(ERR_ok);
+}
