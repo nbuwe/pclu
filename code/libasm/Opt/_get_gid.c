b9a72cb5046f4e567e814ce9a7868c8c0d5620d8
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-27 18:38:28 +0300

    libasm: _get_gid - ANSIfy.  Fix to match reality.
    The Clu declaration in etc.spc lists two return values, like _get_uid,
    so make the C implementation match that specification.

--- code/libasm/Opt/_get_gid.c
+++ code/libasm/Opt/_get_gid.c
@@ -1,18 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _get_gid.c,v 1.2 91/06/06 13:45:49 dcurtis Exp $";
-#endif
-/* $Log:	_get_gid.c,v $
- * Revision 1.2  91/06/06  13:45:49  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:20:27  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -22,14 +8,17 @@ static char rcsid[] = "$Header: _get_gid.c,v 1.2 91/06/06 13:45:49 dcurtis Exp $
 
 #include "pclu_err.h"
 #include "pclu_sys.h"
-#include <sys/types.h>
 
-errcode _get_gid(ans)
-CLUREF *ans;
+#include <unistd.h>
+
+
+/*
+ * Returns real group ID, effective group ID.
+ */
+errcode
+_get_gid(CLUREF *gid, CLUREF *egid)
 {
-gid_t gid;
-
-	gid = getgid();
-	ans->num = gid;
-	signal(ERR_ok);
-	}
+    gid->num = getgid();
+    egid->num = getegid();
+    signal(ERR_ok);
+}
