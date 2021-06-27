d447853dae4b30514807a676330f120bf8e678eb
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 02:29:11 +0300

    libasm: _exit, quit_ - ANSIfy.  Make quit() just call _exit(0).

--- code/libasm/Opt/_exit.c
+++ code/libasm/Opt/_exit.c
@@ -1,18 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _exit.c,v 1.2 91/06/06 13:44:33 dcurtis Exp $";
-#endif
-/* $Log:	_exit.c,v $
- * Revision 1.2  91/06/06  13:44:33  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:20:19  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -22,13 +8,18 @@ static char rcsid[] = "$Header: _exit.c,v 1.2 91/06/06 13:44:33 dcurtis Exp $";
 
 #include "pclu_err.h"
 #include "pclu_sys.h"
+#undef _exit /* _OPexit */
 
-#undef _exit
+#include <unistd.h>
 
-errcode _OPexit(status)
-CLUREF status;
+errcode _chanOP_save_tty(void);
+errcode _cleanup_(void);
+
+
+errcode
+_OPexit(CLUREF status)
 {
-	_cleanup_();
-	_chanOP_save_tty();
-	_exit(status.num);
-	}
+    _cleanup_();
+    _chanOP_save_tty();
+    _exit(status.num);
+}
--- code/libasm/Opt/quit_.c
+++ code/libasm/Opt/quit_.c
@@ -1,21 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: quit_.c,v 1.3 91/06/06 13:55:42 dcurtis Exp $";
-#endif
-/* $Log:	quit_.c,v $
- * Revision 1.3  91/06/06  13:55:42  dcurtis
- * added copyright notice
- * 
- * Revision 1.2  91/06/03  17:23:19  root
- * sparcstation compatibility: int->CLUREF
- * 
- * Revision 1.1  91/02/04  23:21:37  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -25,10 +8,18 @@ static char rcsid[] = "$Header: quit_.c,v 1.3 91/06/06 13:55:42 dcurtis Exp $";
 
 #include "pclu_err.h"
 #include "pclu_sys.h"
+#undef _exit /* _OPexit */
 
-errcode quit_()
+// __attribute__((noreturn))
+errcode _OPexit(CLUREF status);
+
+
+errcode
+quit_()
 {
-	_cleanup_();
-	_chanOP_save_tty();
-	_exit(CLU_0);
-	}
+    _OPexit(CLU_0);
+    /* NOTREACHED */
+
+    /* pacify the compiler */
+    return ERR_ok;
+}
