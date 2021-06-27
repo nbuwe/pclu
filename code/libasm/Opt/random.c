1e58087f1504df30bf9f59361530dbcbcea4cb25
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 01:37:18 +0300

    libasm: random.c - ANSIfy, prettify.  Same object code is generated.

--- code/libasm/Opt/random.c
+++ code/libasm/Opt/random.c
@@ -1,20 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: random.c,v 1.3 91/07/18 14:36:18 root Exp $";
-#endif
-/* $Log:	random.c,v $
- * Revision 1.3  91/07/18  14:36:18  root
- * fixed constants to match original
- * 
- * Revision 1.2  91/06/06  13:56:23  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:21:39  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*						*/
@@ -25,8 +9,6 @@ static char rcsid[] = "$Header: random.c,v 1.3 91/07/18 14:36:18 root Exp $";
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-extern char **environ;
-
 static CLUREF random_hi;
 static CLUREF random_lo;
 static int random_init = 0;
@@ -35,50 +17,66 @@ static int random_init = 0;
 #define lo random_lo
 #define init random_init
 
-errcode randomOPreset()
+
+/*
+ * Restore initial seed.
+ */
+errcode
+randomOPreset(void)
 {
-	hi.num = 0Xbfc896df;
-	lo.num = 0X6ab8724a;
+    hi.num = 0xbfc896df;
+    lo.num = 0x6ab8724a;
+    init = true;
+    signal(ERR_ok);
+}
+
+
+/*
+ * Modify current seed.
+ */
+errcode
+randomOPseed(CLUREF x)
+{
+    if (!init) {
+	hi.num = 0xbfc896df;
+	lo.num = 0x6ab8724a;
 	init = true;
-	signal(ERR_ok);
-	}
+    }
+    hi.num = hi.num ^ x.num;
+    signal(ERR_ok);
+}
 
-errcode randomOPseed(x)
-CLUREF x;
+
+/*
+ * 0 <= res < x
+ */
+errcode
+randomOPnext(CLUREF x, CLUREF *ans)
 {
-	if (init == false) {
-		hi.num = 0Xbfc896df;
-		lo.num = 0X6ab8724a;
-		init = true;
-		}
-	hi.num = hi.num ^ x.num;
-	signal(ERR_ok);
-	}
+    CLUREF temphi, templo;	/* aka n1 n0 */
+    CLUREF save;		/* aka n2 */
+    int flag;
 
-errcode randomOPnext(x, ans)
-CLUREF x, *ans;
-{
-CLUREF newhi, newlo;
-CLUREF temphi, templo; /* aka n1 n0 */
-CLUREF save;		/* aka n2 */
-int flag;
+    if (!init) {
+	hi.num = 0xbfc896df;
+	lo.num = 0x6ab8724a;
+	init = true;
+    }
+    templo.num = lo.num;
+    temphi.num = hi.num;
+    lo.num = temphi.num;
+    save.num = temphi.num;
+    flag = temphi.num & 1;
+#if 0				/* not necessary */
+    temphi.num = temphi.num >> 1;
+#endif
+    templo.num = templo.num >> 1;
+    templo.num = (templo.num & 0x7fffffff) | (flag << 31);
+    save.num = save.num ^ templo.num;
+    hi.num = save.num;
+    save.num = save.num >> 2;
 
-	if (init == false) {
-		hi.num = 0Xbfc896df;
-		lo.num = 0X6ab8724a;
-		init = true;
-		}
-	templo.num = lo.num;
-	temphi.num = hi.num;
-	lo.num = temphi.num;
-	save.num = temphi.num;
-	flag = temphi.num & 1;
-/*	temphi.num = temphi.num >> 1; not necessary */
-	templo.num = templo.num >> 1;
-	templo.num = (templo.num & 0x7fffffff) | (flag << 31);
-	save.num = save.num ^ templo.num;
-	hi.num = save.num;
-	save.num = save.num >> 2;
-	ans->num = (save.num & 0X7fffffff) % x.num; /* force result non-negative */
-	signal(ERR_ok);
-	}
+    /* force result non-negative */
+    ans->num = (save.num & 0x7fffffff) % x.num;
+    signal(ERR_ok);
+}
