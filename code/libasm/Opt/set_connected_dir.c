a7c102e77b25d899276d54824c337ff2d61ce2a4
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 18:37:21 +0300

    libasm: set_connected_dir.c - ANSIfy, prettify.
    Strangely, connected_dir() getter is in sysasm, not here in libasm,
    alongside the setter.

--- code/libasm/Opt/set_connected_dir.c
+++ code/libasm/Opt/set_connected_dir.c
@@ -1,20 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: set_connected_dir.c,v 1.3 91/06/06 13:56:48 dcurtis Exp $";
-#endif
-/* $Log:	set_connected_dir.c,v $
- * Revision 1.3  91/06/06  13:56:48  dcurtis
- * added copyright notice
- * 
- * Revision 1.2  91/06/03  16:49:07  root
- * sparcstation compatibility: int->CLUREF
- * 
- * Revision 1.1  91/02/04  23:21:40  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*		IMPLEMENTATION OF		*/
@@ -25,36 +9,66 @@ static char rcsid[] = "$Header: set_connected_dir.c,v 1.3 91/06/06 13:56:48 dcur
 #include "pclu_sys.h"
 
 #include <errno.h>
-extern CLUREF empty_string;
+#include <unistd.h>
 
-errcode set_connected_dir(fs)
-CLUREF fs;
+errcode _OPenviron(CLUREF name, CLUREF *ans);
+errcode file_nameOPcreate(CLUREF dir, CLUREF name, CLUREF suffix, CLUREF other, CLUREF *ret_1);
+errcode file_nameOPget_dir(CLUREF x, CLUREF *ret_1);
+errcode file_name_fill(CLUREF fn, CLUREF dsuffix, CLUREF *ret_1);
+
+
+/*
+ * Change current directory.
+ *
+ * uwe: Strangely, connected_dir() getter is in sysasm, not here in
+ * libasm, alongside the setter.
+ */
+errcode
+set_connected_dir(CLUREF fs)
 {
-errcode err;
-int uerr;
-CLUREF wd, name, home;
+    errcode err;
+    int status;
 
-	if (fs.vec->size == 0) {
-		err = stringOPcons("HOME", CLU_1, CLU_4, &home);
-		if (err != ERR_ok) resignal(err);
-		err = _environ(home, &name);
-		if (err != ERR_ok) resignal(err);
-		}
-	else {
-		err = file_nameOPcreate(fs, empty_string, empty_string, empty_string,
-					&name);
-		if (err == ERR_bad_format) signal(err);
-		if (err != ERR_ok) resignal(err);
-		err = file_name_fill(name, empty_string, &name);
-		if (err != ERR_ok) resignal(err);
-		err = file_nameOPget_dir(name, &name);
-		if (err != ERR_ok) resignal(err);
-		}
-	uerr = chdir(name.str->data);
-	if (uerr != 0) {
-		elist[0] = _unix_erstr(errno);
-		signal(ERR_not_possible);
-		}
-	signal(ERR_ok);
-	}
+    CLUREF dirname;
+    if (fs.str->size == 0) {
+	CLUREF home;
+	stringOPcons("HOME", CLU_1, CLU_4, &home);
+	err = _environ(home, &dirname);
+	if (err != ERR_ok)
+	    goto ex_0;
+    }
+    else {
+	CLUREF fname;
+	err = file_nameOPcreate(fs,
+				CLU_empty_string,
+				CLU_empty_string,
+				CLU_empty_string,
+				&fname);
+	if (err == ERR_bad_format)
+	    signal(err);
+	if (err != ERR_ok)
+	    goto ex_0;
 
+	err = file_name_fill(fname, CLU_empty_string, &fname);
+	if (err != ERR_ok)
+	    goto ex_0;
+
+	err = file_nameOPget_dir(fname, &dirname);
+	if (err != ERR_ok)
+	    goto ex_0;
+    }
+
+    status = chdir(dirname.str->data);
+    if (status != 0) {
+	elist[0] = _unix_erstr(errno);
+	signal(ERR_not_possible);
+    }
+
+    signal(ERR_ok);
+
+  ex_0: {
+	if (err != ERR_failure)
+	    elist[0] = _pclu_erstr(err);
+	signal(ERR_failure);
+    }
+}
