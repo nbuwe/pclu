aa954a34729db9407658ccd31a3d1772ae707477
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 03:25:28 +0300

    libasm: get_argv.c - ANSIfy, prettify.

--- code/libasm/Opt/get_argv.c
+++ code/libasm/Opt/get_argv.c
@@ -1,20 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: get_argv.c,v 1.3 91/06/06 13:53:34 dcurtis Exp $";
-#endif
-/* $Log:	get_argv.c,v $
- * Revision 1.3  91/06/06  13:53:34  dcurtis
- * added copyright notice
- * 
- * Revision 1.2  91/06/03  17:28:44  root
- * sparcstation compatibility: int->CLUREF
- * 
- * Revision 1.1  91/02/04  23:21:22  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*						*/
@@ -22,67 +6,72 @@ static char rcsid[] = "$Header: get_argv.c,v 1.3 91/06/06 13:53:34 dcurtis Exp $
 /*			get_argv		*/
 /*						*/
 
-/* looks at global C-style argv (char **) and   */
-/* creates corresponding clu sequence of strings*/
-/* argv[0] (the name of the invoked progrem)	*/
-/* is omitted.					*/
-/* returns the clu sequence of strings		*/
-
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
+/* sysasm: util.c */
+extern int argc;
 extern char **argv;
+
+/* sysasm: _chan.c - debugger fd? */
 extern long wrpipe;
 
-static long save_argv = 0;
+static CLUREF save_argv;
 
-errcode get_argv(ans)
-CLUREF *ans;
+
+errcode
+get_argv(CLUREF *ans)
 {
-errcode err;
-CLUREF temp;
-CLUREF temp2;
-CLUREF size, len;
-long i;
+    errcode err;
 
-	if (save_argv != 0) {
-		ans->num = save_argv;
-		signal(ERR_ok);
-		}
-	if (wrpipe != 0 || argv[1] == 0) {
-		err = sequenceOPnew(&temp);
-		if (err != ERR_ok) resignal(err);
-		save_argv = temp.num;
-		ans->vec = temp.vec;
-		signal(ERR_ok);
-		}
-	/* find number of strings in argv */
-	size.num = 0;
-	for (i = 1;; i++) {
-		if (argv[i] == 0) break;
-		size.num += 1;
-		}
-	err = sequenceOPfill(size, CLU_0, &temp);
-	if (err != ERR_ok) resignal(err);
-	for(i = 1;; i++) {
-		if (argv[i] == 0) break;
-		len.num = strlen(argv[i]);
-		err = stringOPcons(argv[i], CLU_1, len, &temp2);
-		if (err != ERR_ok) resignal(err);
-		temp.vec->data[i-1] = temp2.num;	
-		}
-	save_argv = temp.num;
-	ans->vec = temp.vec;
+    if (save_argv.vec != NULL) {
+	*ans = save_argv;
 	signal(ERR_ok);
-	}
+    }
 
-/* set_argv modifies the static variable used by get_argv to hold */
-/* the CLU sequence of strings corresponding to argv.  argv, the C */
-/* global, is not affected.  new 9/10/92 dwc */
+    if (wrpipe != 0 || argc <= 1) {
+	err = sequenceOPnew(&save_argv);
+	if (err != ERR_ok)
+	    goto ex_0;
 
-errcode set_argv(args, ans)
-CLUREF args, ans;
+	*ans = save_argv;
+	signal(ERR_ok);
+    }
+
+
+    CLUREF seq;
+    err = sequenceOPnew2(CLUREF_make_num(argc - 1), &seq);
+    if (err != ERR_ok)
+	goto ex_0;
+
+    for (int i = 1; i < argc; ++i) {
+	size_t len = strlen(argv[i]);
+
+	CLUREF arg;
+	stringOPcons(argv[i], CLU_1, CLUREF_make_num(len), &arg);
+	seq.vec->data[i - 1] = arg.num;
+    }
+
+    save_argv = seq;
+    *ans = save_argv;
+    signal(ERR_ok);
+
+  ex_0: {
+	if (err != ERR_failure)
+	    elist[0] = _pclu_erstr(err);
+	signal(ERR_failure);
+    }
+}
+
+
+/*
+ * set_argv() modifies the static variable used by get_argv() to hold
+ * the CLU sequence of strings corresponding to argv.  The C global
+ * variable "argv", is not affected.  new 9/10/92 dwc
+ */
+errcode
+set_argv(CLUREF args)
 {
-	save_argv = args.num;
-	signal(ERR_ok);
-	}
+    save_argv = args;
+    signal(ERR_ok);
+}
