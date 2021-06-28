46fda8c55f9ff91f810a8c54e88b3e994c712a42
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 05:43:23 +0300

    libasm: gc.c - ANSIfy.

--- code/libasm/Opt/gc.c
+++ code/libasm/Opt/gc.c
@@ -1,18 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: gc.c,v 1.2 91/06/06 13:53:23 dcurtis Exp $";
-#endif
-/* $Log:	gc.c,v $
- * Revision 1.2  91/06/06  13:53:23  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:21:16  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -20,21 +6,30 @@ static char rcsid[] = "$Header: gc.c,v 1.2 91/06/06 13:53:23 dcurtis Exp $";
 /*			gc			*/
 /*						*/
 
-#include <stdio.h>
 #include "pclu_err.h"
 #include "pclu_sys.h"
 #undef gc
 
+#include <stdio.h>
+
+/* XXX: stubs in sysasm unimp.c */
+errcode _gcOPgc(void);
 extern int heaplo;
 
-errcode gc(ans)
-CLUREF *ans;
-{
-int old_heaplo;
 
-	fprintf(stderr, "gc$gc returning wrong value\n");
-	old_heaplo = heaplo;
-	_gcOPgc();
-	ans->num = (heaplo - old_heaplo) / 4;
-	signal(ERR_ok);
-	}
+/*
+ * Returns number of (long) words reclaimed.
+ */
+errcode
+gc(CLUREF *ans)
+{
+    int old_heaplo;
+
+    fprintf(stderr, "gc$gc returning wrong value\n");
+
+    old_heaplo = heaplo;
+    _gcOPgc();
+
+    ans->num = (heaplo - old_heaplo) / 4;
+    signal(ERR_ok);
+}
