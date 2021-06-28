4f2f2d2854f2ed41cd57a5319ac5f223558bf8f9
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 16:35:27 +0300

    libasm: _nice.c - ANSIfy, prettify.

--- code/libasm/Opt/_nice.c
+++ code/libasm/Opt/_nice.c
@@ -1,17 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _nice.c,v 1.2 91/06/06 13:48:59 dcurtis Exp $";
-#endif
-/* $Log:	_nice.c,v $
- * Revision 1.2  91/06/06  13:48:59  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:20:47  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*						*/
@@ -23,15 +10,17 @@ static char rcsid[] = "$Header: _nice.c,v 1.2 91/06/06 13:48:59 dcurtis Exp $";
 #include "pclu_sys.h"
 
 #include <errno.h>
+#include <unistd.h>
 
 
-errcode _nice(delta)
-CLUREF delta;
+errcode
+_nice(CLUREF delta)
 {
-int uerr;
-
-	uerr = nice(delta.num);
-	if (uerr == 0) signal(ERR_ok);
+    int status = nice(delta.num);
+    if (status != 0) {
 	elist[0] = _unix_erstr(errno);
 	signal(ERR_not_possible);
-	}
+    }
+
+    signal(ERR_ok);
+}
