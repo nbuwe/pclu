6fe41c3123d99a903502e448d398ae3868b94637
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 15:46:34 +0300

    libasm: _real_time.c, _micro_time.c - ANSIfy.  Use integer arithmetic.

--- code/libasm/Opt/_micro_time.c
+++ code/libasm/Opt/_micro_time.c
@@ -1,18 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _micro_time.c,v 1.2 91/06/06 13:48:47 dcurtis Exp $";
-#endif
-/* $Log:	_micro_time.c,v $
- * Revision 1.2  91/06/06  13:48:47  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:20:46  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -25,19 +11,23 @@ static char rcsid[] = "$Header: _micro_time.c,v 1.2 91/06/06 13:48:47 dcurtis Ex
 
 #include <sys/time.h>
 #undef signal
+
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-errcode _micro_time(ans)
-CLUREF *ans;
-{
-struct timeval tp;
-double temp;
-unsigned long temp2;
+#include <stdint.h>
 
-	gettimeofday(&tp, 0);
-	temp = ((double)tp.tv_sec * 1000000) + tp.tv_usec;
-	temp2 = temp;
-	ans->num = temp2;
-	signal(ERR_ok);
-	}
+
+/*
+ * Return real time in microseconds since unknown event in past.
+ */
+errcode
+_micro_time(CLUREF *ans)
+{
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+
+    /* let it overflow: we're only interested in the lsb's */
+    ans->num = ((uint64_t)tp.tv_sec * 1000 * 1000) + tp.tv_usec;
+    signal(ERR_ok);
+}
--- code/libasm/Opt/_real_time.c
+++ code/libasm/Opt/_real_time.c
@@ -1,18 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _real_time.c,v 1.2 91/06/06 13:50:03 dcurtis Exp $";
-#endif
-/* $Log:	_real_time.c,v $
- * Revision 1.2  91/06/06  13:50:03  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:20:54  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -25,23 +11,23 @@ static char rcsid[] = "$Header: _real_time.c,v 1.2 91/06/06 13:50:03 dcurtis Exp
 
 #include <sys/time.h>
 #undef signal
+
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-errcode _real_time(ans)
-CLUREF *ans;
-{
-struct timeval tp;
-double temp;
-unsigned long temp2;
+#include <stdint.h>
 
-	gettimeofday(&tp, 0);
+
 /*
-	temp = ((double) tp.tv_sec * 1000)  + (tp.tv_usec /1000);
-	temp2 = temp;
-*/
-/* let it overflow: we're only interested in the lsb's */
-	temp2 = (tp.tv_sec * 1000) + (tp.tv_usec/1000);
-	ans->num = temp2;
-	signal(ERR_ok);
-	}
+ * Return real time in milliseconds since unknown event in past.
+ */
+errcode
+_real_time(CLUREF *ans)
+{
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+
+    /* let it overflow: we're only interested in the lsb's */
+    ans->num = ((uint64_t)tp.tv_sec * 1000) + (tp.tv_usec / 1000);
+    signal(ERR_ok);
+}
