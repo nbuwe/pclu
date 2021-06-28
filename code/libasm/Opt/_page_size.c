3e94cb7c3e79ebdb13eb4dc9587a8713ae8e55e2
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 04:39:55 +0300

    libasm: _page_size.c - ANSIfy, prettify.

--- code/libasm/Opt/_page_size.c
+++ code/libasm/Opt/_page_size.c
@@ -1,18 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _page_size.c,v 1.2 91/06/06 13:49:25 dcurtis Exp $";
-#endif
-/* $Log:	_page_size.c,v $
- * Revision 1.2  91/06/06  13:49:25  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:20:50  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
@@ -23,12 +9,15 @@ static char rcsid[] = "$Header: _page_size.c,v 1.2 91/06/06 13:49:25 dcurtis Exp
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-errcode _page_size(ans)
-CLUREF *ans;
-{
-int temp;
+#include <unistd.h>
 
-	temp = getpagesize();
-	ans->num = temp;
-	signal(ERR_ok);
-	}
+
+/*
+ * Returns system page size in bytes.
+ */
+errcode
+_page_size(CLUREF *ans)
+{
+    ans->num = getpagesize();
+    signal(ERR_ok);
+}
