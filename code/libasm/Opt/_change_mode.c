2cfcc312445353e7c20a30ae202d74cc97bc10b4
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 19:26:56 +0300

    libasm: _change_mode.c, _change_owner.c - ANSIfy, prettify.

--- code/libasm/Opt/_change_mode.c
+++ code/libasm/Opt/_change_mode.c
@@ -1,17 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _change_mode.c,v 1.2 91/06/06 13:43:45 dcurtis Exp $";
-#endif
-/* $Log:	_change_mode.c,v $
- * Revision 1.2  91/06/06  13:43:45  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:20:14  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*						*/
@@ -22,25 +9,41 @@ static char rcsid[] = "$Header: _change_mode.c,v 1.2 91/06/06 13:43:45 dcurtis E
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
+#include <sys/stat.h>
 #include <errno.h>
-//extern int errno;
-extern CLUREF empty_string;
 
-errcode _change_mode(fn, mode)
-CLUREF fn, mode;
+errcode file_nameOPunparse(CLUREF x, CLUREF *ret_1);
+errcode file_name_fill(CLUREF fn, CLUREF dsuffix, CLUREF *ret_1);
+
+
+
+errcode
+_change_mode(CLUREF fn, CLUREF mode)
 {
-CLUREF newfn, name;
-int err;
+    errcode err;
+    int status;
 
-	err = file_name_fill(fn, empty_string, &newfn);
-	if (err != ERR_ok) resignal(err);
-	err = file_nameOPunparse(newfn, &name);
-	if (err != ERR_ok) resignal(err);
-	err = chmod(name.vec->data, mode.num);
-	if (err != 0) {
-		elist[0] = _unix_erstr(errno);
-		signal(ERR_not_possible);
-		}
-	signal(ERR_ok);
-	}
+    CLUREF newfn;
+    err = file_name_fill(fn, CLU_empty_string, &newfn);
+    if (err != ERR_ok)
+	goto ex_0;
 
+    CLUREF name;
+    err = file_nameOPunparse(newfn, &name);
+    if (err != ERR_ok)
+	goto ex_0;
+
+    status = chmod(name.str->data, mode.num);
+    if (status != 0) {
+	elist[0] = _unix_erstr(errno);
+	signal(ERR_not_possible);
+    }
+
+    signal(ERR_ok);
+
+  ex_0: {
+	if (err != ERR_failure)
+	    elist[0] = _pclu_erstr(err);
+	signal(ERR_failure);
+    }
+}
--- code/libasm/Opt/_change_owner.c
+++ code/libasm/Opt/_change_owner.c
@@ -1,17 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _change_owner.c,v 1.2 91/06/06 13:43:56 dcurtis Exp $";
-#endif
-/* $Log:	_change_owner.c,v $
- * Revision 1.2  91/06/06  13:43:56  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:20:15  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*						*/
@@ -23,23 +10,40 @@ static char rcsid[] = "$Header: _change_owner.c,v 1.2 91/06/06 13:43:56 dcurtis 
 #include "pclu_sys.h"
 
 #include <errno.h>
-//extern int errno;
-extern CLUREF empty_string;
+#include <unistd.h>
 
-errcode _change_owner(fn, userid, groupid)
-CLUREF fn, userid, groupid;
+errcode file_nameOPunparse(CLUREF x, CLUREF *ret_1);
+errcode file_name_fill(CLUREF fn, CLUREF dsuffix, CLUREF *ret_1);
+
+
+
+errcode
+_change_owner(CLUREF fn, CLUREF userid, CLUREF groupid)
 {
-errcode err;
-CLUREF newfn, name;
+    errcode err;
+    int status;
 
-  	err = file_name_fill(fn, empty_string, &newfn);
-	if (err != ERR_ok) resignal(err);
-	err = file_nameOPunparse(newfn, &name);
-	if (err != ERR_ok) resignal(err);
- 	err = chown(name.str->data, userid.num, groupid.num);
-	if (err != 0) {
-		elist[0] = _unix_erstr(errno);
-		signal(ERR_not_possible);
-		}
-	signal(ERR_ok);
-	}
+    CLUREF newfn;
+    err = file_name_fill(fn, CLU_empty_string, &newfn);
+    if (err != ERR_ok)
+	goto ex_0;
+
+    CLUREF name;
+    err = file_nameOPunparse(newfn, &name);
+    if (err != ERR_ok)
+	goto ex_0;
+
+    status = chown(name.str->data, userid.num, groupid.num);
+    if (status != 0) {
+	elist[0] = _unix_erstr(errno);
+	signal(ERR_not_possible);
+    }
+
+    signal(ERR_ok);
+
+  ex_0: {
+	if (err != ERR_failure)
+	    elist[0] = _pclu_erstr(err);
+	signal(ERR_failure);
+    }
+}
