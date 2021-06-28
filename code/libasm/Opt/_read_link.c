fa02211c1de1a404561b0c25ce46ea1f82fc6bbf
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 21:16:57 +0300

    libasm: _read_link.c - ANSIfy, prettify.  Comment.

--- code/libasm/Opt/_read_link.c
+++ code/libasm/Opt/_read_link.c
@@ -1,17 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _read_link.c,v 1.2 91/06/06 13:49:49 dcurtis Exp $";
-#endif
-/* $Log:	_read_link.c,v $
- * Revision 1.2  91/06/06  13:49:49  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:20:52  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*		IMPLEMENTATION OF		*/
@@ -22,24 +9,52 @@ static char rcsid[] = "$Header: _read_link.c,v 1.2 91/06/06 13:49:49 dcurtis Exp
 #include "pclu_sys.h"
 
 #include <errno.h>
-extern CLUREF empty_string;
+#include <unistd.h>
 
-errcode _read_link(fn, buf, ans)
+errcode file_nameOPunparse(CLUREF x, CLUREF *ret_1);
+errcode file_name_fill(CLUREF fn, CLUREF dsuffix, CLUREF *ret_1);
+
+
+
+errcode
+_read_link(fn, buf, ans)
 CLUREF fn, buf, *ans;
 {
-errcode err;
-int count;
-CLUREF newfn, name;
+    errcode err;
 
-	err = file_name_fill(fn, empty_string, &newfn);
-	if (err != ERR_ok) resignal(err);
-	err = file_nameOPunparse(newfn, &name);
-	if (err != ERR_ok) resignal(err);
-	count = readlink(name.vec->data, buf.vec->data, buf.vec->size);
-	if (err == -1) {
-		elist[0] = _unix_erstr(errno);
-		signal(ERR_not_possible);
-		}
-	ans->num = count;
-	signal(ERR_ok);
-	}
+    CLUREF newfn;
+    err = file_name_fill(fn, CLU_empty_string, &newfn);
+    if (err != ERR_ok)
+	goto ex_0;
+
+    CLUREF name;
+    err = file_nameOPunparse(newfn, &name);
+    if (err != ERR_ok)
+	goto ex_0;
+
+    /*
+     * readlink(2) doesn't append a NUL byte, but clu allocator zeroes
+     * the memory it returns, and _bytevec allocates an extra byte
+     * (not acoounted for by buf.str->size), so there's always a NUL
+     * byte after the string in the buffer.
+     *
+     * readlink(2) truncates silently.  "bounds" is not declared to be
+     * signaled by _read_link() but if we wanted to detect it we would
+     * need to pass size+1 to readlink to see if the extra terminator
+     * byte is overwritten (and fix it up if it is).
+     */
+    ssize_t nbytes = readlink(name.str->data, buf.str->data, buf.str->size);
+    if (nbytes < 0) {
+	elist[0] = _unix_erstr(errno);
+	signal(ERR_not_possible);
+    }
+
+    ans->num = nbytes;
+    signal(ERR_ok);
+
+  ex_0: {
+	if (err != ERR_failure)
+	    elist[0] = _pclu_erstr(err);
+	signal(ERR_failure);
+    }
+}
