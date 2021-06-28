de9b5764107680bf7cecafaba009f9093d6e2065
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-28 15:53:58 +0300

    libasm: _get_priority.c, _set_priority.c - ANSIfy, prettify.

--- code/libasm/Opt/_set_priority.c
+++ code/libasm/Opt/_set_priority.c
@@ -1,37 +1,31 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: _set_priority.c,v 1.2 91/06/06 13:50:53 dcurtis Exp $";
-#endif
-/* $Log:	_set_priority.c,v $
- * Revision 1.2  91/06/06  13:50:53  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:21:00  mtv
- * Initial revision
- * 
- */
-
 
 /*						*/
 /*						*/
 /*		IMPLEMENTATION OF		*/
-/*			_set_priority			*/
+/*			_set_priority		*/
 /*						*/
+
+#include <sys/time.h>
+#include <sys/resource.h>
+#undef signal
 
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
 #include <errno.h>
 
-errcode _set_priority(which, who, prio)
-CLUREF which, who, prio;
-{
-int err;
 
-	err = setpriority(which.num, who.num, prio.num);
-	if (err == 0) signal(ERR_ok);
+errcode
+_set_priority(CLUREF which, CLUREF who, CLUREF prio)
+{
+    int status;
+
+    status = setpriority(which.num, who.num, prio.num);
+    if (status < 0) {
 	elist[0] = _unix_erstr(errno);
 	signal(ERR_not_possible);
-	}
+    }
+
+    signal(ERR_ok);
+}
