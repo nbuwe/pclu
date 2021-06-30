d1ddf6ea9053f2ccc41846fde0ec3acdf198878a
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-30 04:55:19 +0300

    libasm: _sleep.c, sleep.c - ANSIfy, prettify.  Same object code is generated.

--- code/libasm/Opt/sleep.c
+++ code/libasm/Opt/sleep.c
@@ -1,20 +1,4 @@
-
 /* Copyright Massachusetts Institute of Technology 1990,1991 */
-
-#ifndef lint
-static char rcsid[] = "$Header: /pm/src/site/pclu/code/libasm/RCS/sleep.c,v 1.3 91/08/29 16:07:33 dcurtis Exp $";
-#endif
-/* $Log:	sleep.c,v $
- * Revision 1.3  91/08/29  16:07:33  dcurtis
- * fixed casting on arguments to sigvec,setitimer
- * 
- * Revision 1.2  91/06/06  13:57:00  dcurtis
- * added copyright notice
- * 
- * Revision 1.1  91/02/04  23:21:41  mtv
- * Initial revision
- * 
- */
 
 /*						*/
 /*						*/
@@ -22,46 +6,59 @@ static char rcsid[] = "$Header: /pm/src/site/pclu/code/libasm/RCS/sleep.c,v 1.3 
 /*			sleep			*/
 /*						*/
 
+#include <sys/time.h>
 #include <signal.h>
 #undef signal
-#include <sys/time.h>
+
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-extern void sleep_alarm();
+void sleep_alarm(int);
 
-unsigned int sleep(isecs)
-unsigned int isecs;
+
+unsigned
+int sleep(unsigned int isecs)
 {
-int uerr;
-int omask, omask2;
-struct sigaction vec;
-int which;
-struct itimerval value;
-CLUREF secs;
+    int uerr;
+    int omask, omask2;
+    struct sigaction vec;
+    int which;
+    struct itimerval value;
+    CLUREF secs;
 
-	secs.num = isecs;
-	if (secs.num == 0) signal(ERR_ok);
-	vec.sa_handler = sleep_alarm;
-	sigfillset(&vec.sa_mask);
-	vec.sa_flags = true;
-	uerr = sigaction(SIGALRM, &vec, (struct sigaction*)0);
-	omask = sigblock(sigmask(SIGALRM));
-	which = ITIMER_REAL;
-	value.it_interval.tv_sec = 0;
-	value.it_interval.tv_usec = 0;
-	value.it_value.tv_sec = secs.num;
-	value.it_value.tv_usec = 0;
-	uerr = setitimer(which, &value, (struct itimerval*)0);
-	if (uerr != 0) signal(ERR_ok);
-	omask2 = sigpause(omask);
-	sigsetmask(omask);
-	value.it_value.tv_sec = 0;
-	setitimer(which, &value, (struct itimerval*)0);
+    secs.num = isecs;
+    if (secs.num == 0)
 	signal(ERR_ok);
-	}
 
-void sleep_alarm()
+    vec.sa_handler = sleep_alarm;
+    sigfillset(&vec.sa_mask);
+    vec.sa_flags = true;
+
+    uerr = sigaction(SIGALRM, &vec, NULL);
+
+    omask = sigblock(sigmask(SIGALRM));
+    which = ITIMER_REAL;
+    value.it_interval.tv_sec = 0;
+    value.it_interval.tv_usec = 0;
+    value.it_value.tv_sec = secs.num;
+    value.it_value.tv_usec = 0;
+    uerr = setitimer(which, &value, NULL);
+    if (uerr != 0)
+	signal(ERR_ok);
+
+    omask2 = sigpause(omask);
+
+    sigsetmask(omask);
+    value.it_value.tv_sec = 0;
+    // value.it_value.tv_usec = 0;
+    setitimer(which, &value, NULL);
+    signal(ERR_ok);
+}
+
+
+void
+sleep_alarm(int sig)
 {
-	return;
-	}
+    CLU_NOREF(sig);
+    return;
+}
