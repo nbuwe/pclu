609db663e47659c4c04ba94630e3322158dc2b32
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-06-30 05:16:32 +0300

    libasm: sleep.c - Delegate to _sleep(millis).
    FIXME: This function overrides sleep(3) from libc.
    
    It's probably better to fix up the clu name to avoid the name clash as
    the compiler already does for C keywords.
    
    This code could almost dodge this problem b/c 3rd party C code calling
    sleep(3) and clu code calling sleep() are ABI compatible if small
    structs are passed unboxed.  But the problem is that the libc version
    and clu version have different return values, so this pun never fully
    worked as expected for the non-default case (if sleep was interrupted).

--- code/libasm/Opt/sleep.c
+++ code/libasm/Opt/sleep.c
@@ -13,52 +13,27 @@
 #include "pclu_err.h"
 #include "pclu_sys.h"
 
-void sleep_alarm(int);
+errcode _sleep(CLUREF millis);
 
 
+/*
+ * Forward to _sleep() to avoid code duplication, it differs only in
+ * the units of the argument and was exactly the same code otherwise.
+ *
+ * FIXME: This function overrides sleep(3) from libc.
+ *
+ * It's probably better to fix up the clu name to avoid the name clash
+ * as the compiler already does for C keywords.
+ *
+ * This code could almost dodge this problem b/c 3rd party C code
+ * calling sleep(3) and clu code calling sleep() are ABI compatible if
+ * small structs are passed unboxed.  But the problem is that the libc
+ * version and clu version have different return values, so this pun
+ * never fully worked as expected for the non-default case (if sleep
+ * was interrupted).
+ */
 unsigned int
-sleep(unsigned int isecs)
+sleep(unsigned int secs)
 {
-    int uerr;
-    int omask, omask2;
-    struct sigaction vec;
-    int which;
-    struct itimerval value;
-    CLUREF secs;
-
-    secs.num = isecs;
-    if (secs.num == 0)
-	signal(ERR_ok);
-
-    vec.sa_handler = sleep_alarm;
-    sigfillset(&vec.sa_mask);
-    vec.sa_flags = true;
-
-    uerr = sigaction(SIGALRM, &vec, NULL);
-
-    omask = sigblock(sigmask(SIGALRM));
-    which = ITIMER_REAL;
-    value.it_interval.tv_sec = 0;
-    value.it_interval.tv_usec = 0;
-    value.it_value.tv_sec = secs.num;
-    value.it_value.tv_usec = 0;
-    uerr = setitimer(which, &value, NULL);
-    if (uerr != 0)
-	signal(ERR_ok);
-
-    omask2 = sigpause(omask);
-
-    sigsetmask(omask);
-    value.it_value.tv_sec = 0;
-    // value.it_value.tv_usec = 0;
-    setitimer(which, &value, NULL);
-    signal(ERR_ok);
-}
-
-
-void
-sleep_alarm(int sig)
-{
-    CLU_NOREF(sig);
-    return;
+    return _sleep(CLUREF_make_num(secs * 1000));
 }
