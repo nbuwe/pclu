cb43f2f00663192f276d685b7f9b3b2cd43bfc4b
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-10-16 05:38:21 +0300

    debug: libinfo.c - Reindent.  Xref debug_c.c.
    debug_c.c defines the same struct as "frame" but slightly differently.

--- debug/libinfo.c
+++ debug/libinfo.c
@@ -1,25 +1,29 @@
-
 /*	debugging information so that system functions */
 /*		can be invoked			       */
 
 #include "pclu_err.h"
 #include "pclu_sys.h"
+
+/*
+ * XXX: debug_c.c defines this as "frame" but slightly differently.
+ */
 typedef struct {
-	long	hdr;
-	long	count;
-	char  *name;
-	char  *fname;
-	bool   	is_iter;
-	long    	is_parmd;
-	errcode	(*addr)();
-	long	nargs;
-	Vlist0   *vals; 
-	siglist0 *sigs;
-	Vlist0 *locals;
-	Vlist0 *owns;
-	Vlist0 *ptowns;
-	Vlist0 *popowns;
-	Slist0 *type_formals;
-	Slist0 *op_formals;
-	} dbg_info_generic;
+    long hdr;
+    long count;
+    char *name;
+    char *fname;
+    bool is_iter;
+    long is_parmd;
+    errcode (*addr)();
+    long nargs;
+    Vlist0 *vals;
+    siglist0 *sigs;
+    Vlist0 *locals;
+    Vlist0 *owns;
+    Vlist0 *ptowns;
+    Vlist0 *popowns;
+    Slist0 *type_formals;
+    Slist0 *op_formals;
+} dbg_info_generic;
+
 #include "libinfo.h"
