b864328c9a8b8213d3ac82deb7ec8e76c8949b83
"Valery Ushakov" <uwe@stderr.spb.ru>
2021-10-18 01:43:53 +0300

    Constify pointers in dbg_info.

--- debug/libinfo.c
+++ debug/libinfo.c
@@ -10,20 +10,20 @@
 typedef struct {
     long hdr;
     long count;
-    char *name;
-    char *fname;
+    const char *name;
+    const char *fname;
     bool is_iter;
     long is_parmd;
     errcode (*addr)();
     long nargs;
-    Vlist0 *vals;
-    siglist0 *sigs;
-    Vlist0 *locals;
-    Vlist0 *owns;
-    Vlist0 *ptowns;
-    Vlist0 *popowns;
-    Slist0 *type_formals;
-    Slist0 *op_formals;
+    const Vlist0 *vals;
+    const siglist0 *sigs;
+    const Vlist0 *locals;
+    const Vlist0 *owns;
+    const Vlist0 *ptowns;
+    const Vlist0 *popowns;
+    const Slist0 *type_formals;
+    const Slist0 *op_formals;
 } dbg_info_generic;
 
 #include "libinfo.h"
